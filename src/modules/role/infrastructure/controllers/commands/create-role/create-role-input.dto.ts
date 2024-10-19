120564a7e5dcd5be4fa3a4e28ab279ce23a4fe08
2314-RAustin <0315108515@miutt.edu.mx>
2024-10-19 00:32:13 -0700

    feat(create-role): Update validation length limits for role name and description using constants

--- src/modules/role/infrastructure/controllers/commands/create-role/create-role-input.dto.ts
+++ src/modules/role/infrastructure/controllers/commands/create-role/create-role-input.dto.ts
@@ -1,14 +1,21 @@
 import { IsNotEmpty, IsOptional, IsString, Length } from 'class-validator';
 
+import {
+  MAX_ROLE_NAME_LENGTH,
+  MIN_ROLE_NAME_LENGTH,
+  MAX_ROLE_DESCRIPTION_LENGTH,
+  MIN_ROLE_DESCRIPTION_LENGTH,
+} from '@role/domain/constants/general-rules';
+
 export class CreateRoleInput {
   @IsString()
   @IsNotEmpty()
-  @Length(3, 50)
+  @Length(MAX_ROLE_NAME_LENGTH, MIN_ROLE_NAME_LENGTH)
   public readonly name: string;
 
   @IsString()
   @IsNotEmpty()
   @IsOptional()
-  @Length(5, 200)
+  @Length(MAX_ROLE_DESCRIPTION_LENGTH, MIN_ROLE_DESCRIPTION_LENGTH)
   public readonly description: string;
 }
