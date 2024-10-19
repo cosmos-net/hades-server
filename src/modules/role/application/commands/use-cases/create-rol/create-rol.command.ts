588bc4b51068746ad233d914b32e948dc91ebae9
2314-RAustin <0315108515@miutt.edu.mx>
2024-10-19 00:30:43 -0700

    feat(create-role): Implement ICommand interface in CreateRoleCommand class

--- src/modules/role/application/commands/use-cases/create-rol/create-rol.command.ts
+++ src/modules/role/application/commands/use-cases/create-rol/create-rol.command.ts
@@ -1,4 +1,6 @@
-export class CreateRoleCommand {
+import { ICommand } from '@nestjs/cqrs';
+
+export class CreateRoleCommand implements ICommand {
   public readonly uuid: string;
   public readonly name: string;
   public readonly description: string;
