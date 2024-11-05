cd3e2ed2ab48ba8b69ad9387efd706ec5b9b4fd9
Roberto Austin <36521586+2314-RAustin@users.noreply.github.com>
2024-11-04 21:55:53 -0800

    feat(role): add update role domain service and enhance role model re-description logic

--- src/modules/role/domain/contracts/role-repository.contract.ts
+++ src/modules/role/domain/contracts/role-repository.contract.ts
@@ -3,4 +3,5 @@ import { RoleModel } from '@role/domain/models/role.model';
 export interface IRoleRepositoryContract {
   isNameAvailable(name: string): Promise<boolean>;
   persist(role: RoleModel): Promise<RoleModel>;
+  getOneBy(UUID: string): Promise<RoleModel>;
 }
