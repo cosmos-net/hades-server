718d9ba7a4d69a1e290ade66d4154b9b6a5f5c92
2314-RAustin <0315108515@miutt.edu.mx>
2024-11-19 21:49:32 -0800

    feat: :sparkles: add profile and account constants for validation rules

---
+++ src/modules/user/domain/constants/general-rules.ts
@@ -0,0 +1,57 @@
+export const MAX_PROFILE_NAME_LENGTH = 100;
+export const MIN_PROFILE_NAME_LENGTH = 2;
+
+export const MAX_PROFILE_LAST_NAME_LENGTH = 100;
+export const MIN_PROFILE_LAST_NAME_LENGTH = 2;
+
+export const MAX_PROFILE_SECOND_LAST_NAME_LENGTH = 100;
+export const MIN_PROFILE_SECOND_LAST_NAME_LENGTH = 2;
+
+export const MAX_PROFILE_PHONE_NUMBER_LENGTH = 13;
+export const MIN_PROFILE_PHONE_NUMBER_LENGTH = 9;
+
+export const PROFILE_ADDRESS = {
+  STREET: {
+    MAX_LENGTH: 100,
+    MIN_LENGTH: 2,
+  },
+  EXT_NUMBER: {
+    MAX_LENGTH: 10,
+    MIN_LENGTH: 1,
+  },
+  INT_NUMBER: {
+    MAX_LENGTH: 10,
+    MIN_LENGTH: 1,
+  },
+  NEIGHBORHOOD: {
+    MAX_LENGTH: 100,
+    MIN_LENGTH: 2,
+  },
+  ZIP_CODE: {
+    MAX_LENGTH: 10,
+    MIN_LENGTH: 2,
+  },
+  CITY: {
+    MAX_LENGTH: 100,
+    MIN_LENGTH: 2,
+  },
+  STATE: {
+    MAX_LENGTH: 100,
+    MIN_LENGTH: 2,
+  },
+  COUNTRY: {
+    MAX_LENGTH: 100,
+    MIN_LENGTH: 2,
+  },
+};
+
+export enum ProfileGenderEnum {
+  MAN = 'MAN',
+  WOMAN = 'WOMAN',
+}
+
+export const MAX_ACCOUNT_USER_NAME_LENGTH = 100;
+export const MIN_ACCOUNT_USER_NAME_LENGTH = 5;
+
+export const MAX_ACCOUNT_PASSWORD_LENGTH = 100;
+export const MIN_ACCOUNT_PASSWORD_LENGTH = 5;
