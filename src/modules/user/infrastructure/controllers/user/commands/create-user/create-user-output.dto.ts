8a9e37e672ce7b5143a2bdeacbe1f5984aa2dfa5
2314-RAustin <0315108515@miutt.edu.mx>
2024-11-19 21:50:02 -0800

    feat: :sparkles: add CreateUserOutput DTO for user creation response structure

---
+++ src/modules/user/infrastructure/controllers/user/commands/create-user/create-user-output.dto.ts
@@ -0,0 +1,29 @@
+import { ProfileGenderEnum } from '@user/domain/constants/general-rules';
+
+export class CreateUserOutputDto {
+  public readonly user: {
+    id: string;
+    uuid: string;
+    status: string;
+  };
+
+  public readonly account: {
+    username: string;
+    email: string;
+  };
+
+  public readonly profile: {
+    name: string;
+    lastName: string;
+    secondLastName: string;
+    phoneNumber: string;
+    gender: ProfileGenderEnum;
+    address: {
+      street: string;
+      city: string;
+      state: string;
+      country: string;
+      zipCode: string;
+    };
+  };
+}
