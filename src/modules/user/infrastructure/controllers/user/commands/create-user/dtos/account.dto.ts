2ef9fa25a7395a20f7e1b900e905af03bbabd8ff
2314-RAustin <0315108515@miutt.edu.mx>
2024-11-19 21:49:40 -0800

    feat: :sparkles: implement CreateUserInput DTO and related account, profile, and address DTOs for user creation

---
+++ src/modules/user/infrastructure/controllers/user/commands/create-user/dtos/account.dto.ts
@@ -0,0 +1,25 @@
+import { IsString, IsNotEmpty, Length, IsEmail } from 'class-validator';
+
+import {
+  MAX_ACCOUNT_USER_NAME_LENGTH,
+  MIN_ACCOUNT_USER_NAME_LENGTH,
+  MAX_ACCOUNT_PASSWORD_LENGTH,
+  MIN_ACCOUNT_PASSWORD_LENGTH,
+} from '@user/domain/constants/general-rules';
+
+export class AccountDTO {
+  @IsString()
+  @IsNotEmpty()
+  @Length(MIN_ACCOUNT_USER_NAME_LENGTH, MAX_ACCOUNT_USER_NAME_LENGTH)
+  public readonly username: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsEmail()
+  public readonly email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Length(MIN_ACCOUNT_PASSWORD_LENGTH, MAX_ACCOUNT_PASSWORD_LENGTH)
+  public readonly password: string;
+}
