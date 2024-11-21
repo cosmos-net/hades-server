a3167d868b3049d119b2a0f55968facd5c6fa116
2314-RAustin <0315108515@miutt.edu.mx>
2024-11-20 21:42:41 -0800

    feat: :sparkles: implement UpdateUserController and associated DTOs for user update commands

---
+++ src/modules/user/infrastructure/controllers/user/commands/update-user/dtos/profile.dto.ts
@@ -0,0 +1,61 @@
+import { Type } from 'class-transformer';
+import {
+  IsString,
+  IsNotEmpty,
+  IsOptional,
+  Length,
+  IsEnum,
+  ValidateNested,
+  IsArray,
+  ArrayNotEmpty,
+} from 'class-validator';
+
+import {
+  MAX_PROFILE_LAST_NAME_LENGTH,
+  MIN_PROFILE_LAST_NAME_LENGTH,
+  MAX_PROFILE_SECOND_LAST_NAME_LENGTH,
+  MIN_PROFILE_SECOND_LAST_NAME_LENGTH,
+  MAX_PROFILE_NAME_LENGTH,
+  MIN_PROFILE_NAME_LENGTH,
+  MAX_PROFILE_PHONE_NUMBER_LENGTH,
+  MIN_PROFILE_PHONE_NUMBER_LENGTH,
+  ProfileGenderEnum,
+} from '@user/domain/constants/general-rules';
+import { AddressDTO } from '@user/infrastructure/controllers/user/commands/update-user/dtos/address.dto';
+
+export class ProfileDTO {
+  @IsOptional()
+  @IsArray()
+  @ArrayNotEmpty()
+  @IsString({ each: true })
+  @Length(MIN_PROFILE_NAME_LENGTH, MAX_PROFILE_NAME_LENGTH, { each: true })
+  public readonly names?: string[];
+
+  @IsOptional()
+  @IsString()
+  @IsNotEmpty()
+  @Length(MIN_PROFILE_LAST_NAME_LENGTH, MAX_PROFILE_LAST_NAME_LENGTH)
+  public readonly lastName?: string;
+
+  @IsOptional()
+  @IsString()
+  @Length(MIN_PROFILE_SECOND_LAST_NAME_LENGTH, MAX_PROFILE_SECOND_LAST_NAME_LENGTH)
+  public readonly secondLastName?: string;
+
+  @IsOptional()
+  @IsString()
+  @IsNotEmpty()
+  @Length(MIN_PROFILE_PHONE_NUMBER_LENGTH, MAX_PROFILE_PHONE_NUMBER_LENGTH)
+  public readonly phoneNumber?: string;
+
+  @IsOptional()
+  @IsEnum(ProfileGenderEnum)
+  @IsNotEmpty()
+  public readonly gender?: ProfileGenderEnum;
+
+  @IsOptional()
+  @IsNotEmpty()
+  @ValidateNested()
+  @Type(() => AddressDTO)
+  public readonly address?: AddressDTO;
+}
