86182a1590047904a279ef23f0df1d7eab18b736
Jesús Crespo <crespojesus9@gmail.com>
2024-11-21 20:20:53 -0800

    feat: :sparkles: user-model create event with uuid and status added

--- src/modules/user/domain/models/user.model.ts
+++ src/modules/user/domain/models/user.model.ts
@@ -1,7 +1,22 @@
 import { AggregateRoot } from '@nestjs/cqrs';
 
+import { UserCreatedEvent } from '@user/domain/events/events-success-domain/user-created.event';
 import { IUserSchema } from '@user/domain/schemas/user.schema';
 
 export class UserModel extends AggregateRoot {
   private readonly _entityRoot: IUserSchema;
+
+  get id(): number {
+    return this._entityRoot.id._value;
+  }
+  get uuid(): string {
+    return this._entityRoot.uuid._value;
+  }
+  get status(): string {
+    return this._entityRoot.status;
+  }
+
+  public create() {
+    this.apply(new UserCreatedEvent(this.uuid, this.status));
+  }
 }
