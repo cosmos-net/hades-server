bdfb2d2b31bea211a6041612085b863cf552426c
2314-RAustin <0315108515@miutt.edu.mx>
2024-11-22 21:20:30 -0800

    feat(user-module): :sparkles: rename IRoleRepositoryContract to IUserRepositoryContract and update user model imports for consistency

--- src/modules/user/domain/contracts/user-repository.contract.ts
+++ src/modules/user/domain/contracts/user-repository.contract.ts
@@ -1,11 +1,11 @@
-import { ListUserModel } from '@role/domain/models/user-list.model';
-import { UserModel } from '@role/domain/models/user.model';
+import { ListUserModel } from '@user/domain/models/user-list.model';
+import { UserModel } from '@user/domain/models/user.model';
 
 import { Criteria } from '@common/domain/criteria/criteria';
 
-export interface IRoleRepositoryContract {
+export interface IUserRepositoryContract {
   isNameAvailable(name: string): Promise<boolean>;
-  persist(role: UserModel): Promise<UserModel>;
+  persist(user: UserModel): Promise<UserModel>;
   getOneBy(UUID: string): Promise<UserModel>;
   destroy(UUID: string): Promise<boolean>;
   archive(UUID: string): Promise<boolean>;
