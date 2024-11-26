ea91ae863b42d722721bf795854487008e42c84a
Jesús Crespo <crespojesus9@gmail.com>
2024-11-25 21:28:37 -0800

    refactor: :art: user-status value object fixed with enum export

--- src/modules/user/domain/value-object/user-status.vo.ts
+++ src/modules/user/domain/value-object/user-status.vo.ts
@@ -1,9 +1,12 @@
-export enum Status {
-  ACTIVE = 'Active',
-  INACTIVE = 'Inactive',
-  PENDING = 'Pending',
-  SUSPENDED = 'Suspended',
-  ARCHIVED = 'Archived',
-  BANNED = 'Banned',
-  UNDERREVIEW = 'UnderReview',
+import { StatusEnum } from '@common/domain/criteria/user-status-enum';
+import { EnumValueObject } from '@common/domain/value-object/types/enum-value-object';
+
+export class UserStatus extends EnumValueObject<StatusEnum> {
+  constructor(value: StatusEnum) {
+    super(value, Object.values(StatusEnum));
+  }
+
+  protected throwErrorForInvalidValue(value: StatusEnum): void {
+    throw new Error(`Invalid status: ${value}`);
+  }
 }
