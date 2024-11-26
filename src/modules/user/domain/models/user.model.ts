4fc455081520cf70590c5831a6b595199e9d7ccf
Jesús Crespo <crespojesus9@gmail.com>
2024-11-25 22:13:19 -0800

    refactor: :art: constructor fixes to integrate charged method

--- src/modules/user/domain/models/user.model.ts
+++ src/modules/user/domain/models/user.model.ts
@@ -1,11 +1,27 @@
 import { AggregateRoot } from '@nestjs/cqrs';
 
+import { StatusEnum } from '@common/domain/criteria/user-status-enum';
+import UUID from '@common/domain/value-object/vos/uuid.vo';
 import { UserCreatedEvent } from '@user/domain/events/events-success-domain/user-created.event';
 import { IUserSchema } from '@user/domain/schemas/user.schema';
+import { IUserSchemaPrimitive } from '@user/domain/schemas/user.schema-primitive';
+import { UserStatus } from '@user/domain/value-object/user-status.vo';
 
 export class UserModel extends AggregateRoot {
   private readonly _entityRoot: IUserSchema;
 
+  constructor(entity: IUserSchemaPrimitive);
+  constructor(uuid: string, status: string);
+  constructor(uuidOrSchema: string | IUserSchemaPrimitive, status?: StatusEnum) {
+    super();
+
+    if (typeof uuidOrSchema === 'object') {
+      this.hydrate(uuidOrSchema);
+    } else {
+      this._entityRoot.uuid = new UUID(uuidOrSchema);
+      if (status) this._entityRoot.status = new UserStatus(status);
+    }
+  }
   get id(): number {
     return this._entityRoot.id._value;
   }
@@ -14,11 +30,15 @@ export class UserModel extends AggregateRoot {
     return this._entityRoot.uuid._value;
   }
 
-  get status(): string {
+  get status(): StatusEnum {
     return this._entityRoot.status.value;
   }
 
   public create() {
     this.apply(new UserCreatedEvent(this.uuid, this.status));
+  }
+  public hydrate(entity: IUserSchemaPrimitive) {
+    this._entityRoot.uuid = new UUID(entity.uuid);
+    this._entityRoot.status = new UserStatus(entity.status);
   }
 }
