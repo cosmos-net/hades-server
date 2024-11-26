2d20a2e6c1ad2761c781ab00d290c7a374a43900
Jesús Crespo <crespojesus9@gmail.com>
2024-11-25 21:28:37 -0800

    refactor: :art: user-status value object fixed with enum export

---
+++ src/modules/common/domain/criteria/user-status-enum.ts
@@ -0,0 +1,9 @@
+export enum StatusEnum {
+  ACTIVE = 'Active',
+  INACTIVE = 'Inactive',
+  PENDING = 'Pending',
+  SUSPENDED = 'Suspended',
+  ARCHIVED = 'Archived',
+  BANNED = 'Banned',
+  UNDERREVIEW = 'UnderReview',
+}
