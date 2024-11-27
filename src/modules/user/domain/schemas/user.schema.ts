341e4f8c6799c3eb9537081a16fc8ee737c7f9bf
Jesús Crespo <crespojesus9@gmail.com>
2024-11-26 20:46:22 -0800

    feat: :sparkles: archivedAt parameter was added into User schema to use it on user model file.

--- src/modules/user/domain/schemas/user.schema.ts
+++ src/modules/user/domain/schemas/user.schema.ts
@@ -1,3 +1,4 @@
+import ArchivedAt from '@common/domain/value-object/vos/archived-at.vo';
 import Id from '@common/domain/value-object/vos/id.vo';
 import UUID from '@common/domain/value-object/vos/uuid.vo';
 import { UserStatus } from '@user/domain/value-object/user-status.vo';
@@ -6,4 +7,5 @@ export interface IUserSchema {
   id: Id;
   uuid: UUID;
   status: UserStatus;
+  archivedAt: ArchivedAt;
 }
