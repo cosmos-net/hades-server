2757e320bb2fbe4c24a8847fece1560d6b9b4c24
2314-RAustin <0315108515@miutt.edu.mx>
2024-11-28 12:06:04 -0800

    feat(remove-role): delete GetRoleQuery and GetRoleUseCase implementations

--- src/modules/role/application/query/use-cases/get-rol/get-role.query.ts
+++
@@ -1,9 +0,0 @@
-import { ICommand } from '@nestjs/cqrs';
-
-export class GetRoleQuery implements ICommand {
-  public readonly uuid: string;
-
-  constructor(root: GetRoleQuery) {
-    this.uuid = root.uuid;
-  }
-}
