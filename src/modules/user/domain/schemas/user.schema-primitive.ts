40603a22e4eb4ec79c86a8be548b2eaf61eff9ca
Jesús Crespo <crespojesus9@gmail.com>
2024-11-27 20:08:55 -0800

    refactor: :art: user schema  and schema primitive parameters added as createdAt, updatedAt and archivedAt

--- src/modules/user/domain/schemas/user.schema-primitive.ts
+++ src/modules/user/domain/schemas/user.schema-primitive.ts
@@ -1,7 +1,13 @@
+import ArchivedAt from '@common/domain/value-object/vos/archived-at.vo';
+import CreatedAt from '@common/domain/value-object/vos/created-at.vo';
+import UpdatedAt from '@common/domain/value-object/vos/updated-at.vo';
 import { StatusEnum } from '@user/domain/enums/user-status-enum';
 
 export interface IUserSchemaPrimitive {
   id: number;
   uuid: string;
   status: StatusEnum;
+  createdAt: CreatedAt;
+  updatedAt: UpdatedAt;
+  archivedAt: ArchivedAt;
 }
