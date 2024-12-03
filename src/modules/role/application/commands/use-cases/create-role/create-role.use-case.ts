5603d3edee8e2ba08f72390989bab15f7358c92c
2314-RAustin <0315108515@miutt.edu.mx>
2024-12-02 21:09:17 -0800

    feat(create-role): inject ROLE_REPOSITORY constant into CreateRoleUseCase for repository integration

--- src/modules/role/application/commands/use-cases/create-role/create-role.use-case.ts
+++ src/modules/role/application/commands/use-cases/create-role/create-role.use-case.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { CommandHandler, EventPublisher, ICommandHandler } from '@nestjs/cqrs';
 
 import { CreateRoleCommand } from '@role/application/commands/use-cases/create-role/create-role.command';
+import { ROLE_REPOSITORY } from '@role/domain/constants/injection-tokens';
 import { IRoleRepositoryContract } from '@role/domain/contracts/role-repository.contract';
 import { RoleModel } from '@role/domain/models/role.model';
 import { CreateRoleDomainService } from '@role/domain/services/create-role.domain-service';
@@ -12,6 +13,7 @@ export class CreateRolUseCase implements ICommandHandler<CreateRoleCommand> {
   constructor(
     private readonly createRoleDomainService: CreateRoleDomainService,
     private readonly publisher: EventPublisher,
+    @Inject(ROLE_REPOSITORY)
     private readonly repository: IRoleRepositoryContract,
   ) {}
 
