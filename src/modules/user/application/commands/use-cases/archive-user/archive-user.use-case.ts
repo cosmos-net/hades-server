5d0bd40d08b0d42d7c3a648d1579e6256904eff9
2314-RAustin <0315108515@miutt.edu.mx>
2024-12-03 21:07:56 -0800

    feat(user-module): inject USER_REPOSITORY constant into user use cases for repository integration

--- src/modules/user/application/commands/use-cases/archive-user/archive-user.use-case.ts
+++ src/modules/user/application/commands/use-cases/archive-user/archive-user.use-case.ts
@@ -1,10 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { CommandHandler, EventPublisher, ICommandHandler } from '@nestjs/cqrs';
-import { ArchiveUserDomainService } from '@user/domain/services/archive-user.domain-service';
 
 import { ArchiveUserCommand } from '@user/application/commands/use-cases/archive-user/archive-user.command';
+import { USER_REPOSITORY } from '@user/domain/constants/injection-tokens';
 import { IUserRepositoryContract } from '@user/domain/contracts/user-repository.contract';
 import { UserModel } from '@user/domain/models/user.model';
+import { ArchiveUserDomainService } from '@user/domain/services/archive-user.domain-service';
 
 @Injectable()
 @CommandHandler(ArchiveUserCommand)
@@ -12,6 +13,7 @@ export class ArchiveUserUseCase implements ICommandHandler<ArchiveUserCommand> {
   constructor(
     private readonly archiveUserDomainService: ArchiveUserDomainService,
     private readonly publisher: EventPublisher,
+    @Inject(USER_REPOSITORY)
     private readonly repository: IUserRepositoryContract,
   ) {}
 
