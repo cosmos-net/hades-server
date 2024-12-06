c30d3caf47cc495848b4215b663d2fdf0c1c99fb
2314-RAustin <0315108515@miutt.edu.mx>
2024-12-05 22:13:40 -0800

    refactor: :art: update IAccountSchemaPrimitive and IAccountSchema to make id optional and add sessions support

--- src/modules/user/domain/schemas/account/account.schema-primitive.ts
+++ src/modules/user/domain/schemas/account/account.schema-primitive.ts
@@ -1,15 +1,18 @@
-export interface IAccountSchemaPrimitive {
-  id: number;
+import { ISessionSchemaPrimitive } from '@user/domain/schemas/session/session.schema-primitive';
+
+export interface IAccountSchemaPrimitives {
+  id?: number;
   uuid: string;
-  username: string[];
-  password: string[];
+  username: string;
+  password: string;
   email: string;
   createdAt: Date;
   updatedAt: Date;
-  destroyedAt: Date;
+  archivedAt: Date;
+  sessions?: ISessionSchemaPrimitive[];
 }
 
 export interface IListAccountSchemaPrimitive {
   total: number;
-  items: IAccountSchemaPrimitive[];
+  items: IAccountSchemaPrimitives[];
 }
