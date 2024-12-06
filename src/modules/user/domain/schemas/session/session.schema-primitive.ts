bab2e997e9d1593cc906dafc56beb1a74f69f4d3
2314-RAustin <0315108515@miutt.edu.mx>
2024-12-05 22:13:31 -0800

    feat: :sparkles: add session schema interfaces for session management

---
+++ src/modules/user/domain/schemas/session/session.schema-primitive.ts
@@ -0,0 +1,26 @@
+export interface ISessionSchemaPrimitive {
+  id: number;
+  uuid: string;
+  sessionId: string;
+  sessionType: string;
+  sessionDuration: number;
+  sessionClosedType: string;
+  token: string;
+  expiresIn: Date;
+  loggedInAt: Date;
+  loggedOutAt: Date;
+  ipAddress: string;
+  refreshToken: string;
+  userAgent: string;
+  failedAttempts: number;
+  origin: string;
+  location: string;
+  createdAt: Date;
+  updatedAt: Date;
+  archivedAt: Date;
+}
+
+export interface IListSessionSchemaPrimitive {
+  total: number;
+  items: ISessionSchemaPrimitive[];
+}
