a6c3970b58c8270d270fb9b4bd3c6079f6b7b365
2314-RAustin <0315108515@miutt.edu.mx>
2024-12-09 20:45:22 -0800

    feat: :sparkles: refactor profile schema to introduce IProfileAddressSchema and IProfileBaseSchema for improved structure and clarity

--- src/modules/user/domain/schemas/profile/profile.schema-primitive.ts
+++ src/modules/user/domain/schemas/profile/profile.schema-primitive.ts
@@ -1,26 +1,31 @@
 import { ProfileGenderEnum } from '@user/domain/constants/general-rules';
 
-export interface IProfileSchemaPrimitive {
-  id?: number;
+export interface IProfileAddressSchema {
+  street: string;
+  extNumber: string;
+  intNumber: string | null;
+  neighborhood: string;
+  zipCode: string;
+  city: string;
+  state: string;
+  country: string;
+}
+
+export interface IProfileBaseSchema {
   uuid: string;
   names: string[];
   lastName: string;
-  secondLastName: string;
+  secondLastName?: string;
   phoneNumber: string;
   gender: ProfileGenderEnum;
-  address: {
-    street: string;
-    extNumber: string;
-    intNumber: string | null;
-    neighborhood: string;
-    zipCode: string;
-    city: string;
-    state: string;
-    country: string;
-  };
+  address: IProfileAddressSchema;
+}
+
+export interface IProfileSchemaPrimitive extends IProfileBaseSchema {
+  id?: number;
   createdAt: Date;
   updatedAt: Date;
-  archivedAt?: Date;
+  archivedAt: Date;
 }
 
 export interface IListProfileSchemaPrimitive {
