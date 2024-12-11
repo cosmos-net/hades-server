3afa7960c29c2e66359cd1be96f0893bffa84a8f
2314-RAustin <0315108515@miutt.edu.mx>
2024-12-10 22:45:50 -0800

    feat(user): :sparkles: refactor CreateUserDomainService and UpdateUserDomainService to support multiple accounts

--- src/modules/user/domain/domain-services/update-user.domain-service.ts
+++ src/modules/user/domain/domain-services/update-user.domain-service.ts
@@ -2,21 +2,25 @@ import DomainException from '@common/domain/exceptions/domain.exception';
 import { UserAggregate } from '@user/domain/aggregates/user.aggregate';
 import { IUserRepositoryContract } from '@user/domain/contracts/user-repository.contract';
 import { ExceptionFactory } from '@user/domain/exceptions/exception.factory';
-import { IAccountBaseSchema } from '@user/domain/schemas/account/account.schema-primitive';
-import { IProfileBaseSchema } from '@user/domain/schemas/profile/profile.schema-primitive';
+import { DeepPartialExceptAccountBaseSchema } from '@user/domain/schemas/account/account.schema-primitive';
+import { DeepPartialProfileBaseSchema } from '@user/domain/schemas/profile/profile.schema-primitive';
 
 export class UpdateUserDomainService {
   constructor(private readonly userRepository: IUserRepositoryContract) {}
 
-  async go(
-    uuid: string,
-    account?: Partial<IAccountBaseSchema>,
-    profile?: Partial<IProfileBaseSchema>,
-  ): Promise<UserAggregate> {
+  async go({
+    uuid,
+    accounts,
+    profile,
+  }: {
+    uuid: string;
+    accounts: DeepPartialExceptAccountBaseSchema;
+    profile: DeepPartialProfileBaseSchema;
+  }): Promise<UserAggregate> {
     try {
       const userAggregate = await this.userRepository.getOneBy(uuid);
 
-      userAggregate.update(account, profile);
+      userAggregate.update(accounts, profile);
 
       await this.userRepository.persist(userAggregate);
 
