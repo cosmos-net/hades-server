75c57cf00eda5d4f22f9dcd46ea19fc38ea6ac5e
2314-RAustin <0315108515@miutt.edu.mx>
2024-12-10 22:46:52 -0800

    feat(user): :sparkles: enhance UpdateUserInput and AccountDTO to support UUIDs and multiple accounts

--- src/modules/user/infrastructure/controllers/user/commands/update-user/update-user-input.dto.ts
+++ src/modules/user/infrastructure/controllers/user/commands/update-user/update-user-input.dto.ts
@@ -1,14 +1,17 @@
 import { Type } from 'class-transformer';
-import { IsOptional, ValidateNested } from 'class-validator';
+import { IsOptional, IsUUID, ValidateNested } from 'class-validator';
 
 import { AccountDTO } from '@user/infrastructure/controllers/user/commands/update-user/dtos/account.dto';
 import { ProfileDTO } from '@user/infrastructure/controllers/user/commands/update-user/dtos/profile.dto';
 
 export class UpdateUserInput {
+  @IsUUID()
+  public readonly uuid: string;
+
   @IsOptional()
-  @ValidateNested()
+  @ValidateNested({ each: true })
   @Type(() => AccountDTO)
-  public readonly account?: AccountDTO;
+  public readonly account?: AccountDTO[];
 
   @IsOptional()
   @ValidateNested()
