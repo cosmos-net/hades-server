4a01937579fa1ce516f3efc33019e5077bf59ea8
2314-RAustin <0315108515@miutt.edu.mx>
2024-12-10 22:47:11 -0800

    feat(user): :sparkles: implement transaction management in UserTypeormRepository for persisting user aggregates

--- src/modules/user/infrastructure/persistence/typeorm/repositories/user-typeorm.repository.ts
+++ src/modules/user/infrastructure/persistence/typeorm/repositories/user-typeorm.repository.ts
@@ -1,12 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Repository, EntityManager } from 'typeorm';
 
 import { Criteria } from '@common/domain/criteria/criteria';
 import { TypeormRepository } from '@common/infrastructure/persistence/typeorm/typeorm-repository';
+import { UserAggregate } from '@user/domain/aggregates/user.aggregate';
 import { IUserRepositoryContract } from '@user/domain/contracts/user-repository.contract';
+import { AccountModel } from '@user/domain/models/account/account.model';
+import { ProfileModel } from '@user/domain/models/profile/profile.model';
 import { ListUserModel } from '@user/domain/models/user/user-list.model';
 import { UserModel } from '@user/domain/models/user/user.model';
+import { AccountEntity } from '@user/infrastructure/persistence/typeorm/entities/account.entity';
+import { ProfileEntity } from '@user/infrastructure/persistence/typeorm/entities/profile.entity';
 import { UserEntity } from '@user/infrastructure/persistence/typeorm/entities/user.entity';
 
 @Injectable()
@@ -17,12 +22,33 @@ export class UserTypeormRepository
   constructor(
     @InjectRepository(UserEntity)
     private readonly repository: Repository<UserEntity>,
+    private readonly entityManager: EntityManager,
   ) {
     super();
   }
 
-  persist<UserModel>(model: UserModel): Promise<UserModel> {
-    return this.repository.save(model);
+  async persist(userAggregate: UserAggregate): Promise<UserAggregate> {
+    return await this.entityManager.transaction(async (manager: EntityManager) => {
+      const savedUser = await manager.save(UserEntity, userAggregate.userModel.toPrimitives());
+
+      const savedProfile = await manager.save(
+        ProfileEntity,
+        userAggregate.profileModel.toPrimitives(),
+      );
+
+      const savedAccounts = await manager.save(
+        AccountEntity,
+        userAggregate.accountsModel.map((account) => account.toPrimitives()),
+      );
+
+      userAggregate.hydrate({
+        userModel: new UserModel(savedUser),
+        profileModel: new ProfileModel(savedProfile),
+        accountsModel: savedAccounts.map((account) => new AccountModel(account)),
+      });
+
+      return userAggregate;
+    });
   }
 
   async archive(uuid: string): Promise<boolean> {
@@ -37,12 +63,26 @@ export class UserTypeormRepository
     return result.affected > 0;
   }
 
-  async getOneBy(uuid: string): Promise<UserModel> {
-    const entity = await this.repository.findOneBy({ uuid });
+  async getOneBy(uuid: string): Promise<UserAggregate> {
+    const entity = await this.repository.findOne({
+      where: { uuid },
+      relations: {
+        accounts: true,
+        profile: true,
+      },
+    });
 
-    const model = new UserModel(entity);
+    const user = new UserModel(entity);
+    const profile = new ProfileModel(entity.profile);
+    const accounts = entity.accounts.map((account) => new AccountModel(account));
 
-    return model;
+    const userAggregate = new UserAggregate({
+      userModel: user,
+      profileModel: profile,
+      accountsModel: accounts,
+    });
+
+    return userAggregate;
   }
 
   async matching(criteria: Criteria): Promise<ListUserModel> {
