c4036a4d28afd27bf6e18e2ae28db58516d283fe
2314-RAustin <0315108515@miutt.edu.mx>
2024-12-10 22:56:29 -0800

    feat(user): :sparkles: update SessionEntity to use dynamic length constants for session fields

--- src/modules/user/infrastructure/persistence/typeorm/entities/session.entity.ts
+++ src/modules/user/infrastructure/persistence/typeorm/entities/session.entity.ts
@@ -1,6 +1,7 @@
 import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
 
 import { TypeormBaseEntity } from '@common/infrastructure/persistence/typeorm/entities/typeorm-base-entity';
+import { SESSION } from '@user/domain/constants/general-rules';
 import { ISessionSchemaPrimitive } from '@user/domain/schemas/session/session.schema-primitive';
 import { AccountEntity } from '@user/infrastructure/persistence/typeorm/entities/account.entity';
 
@@ -12,19 +13,19 @@ export class SessionEntity extends TypeormBaseEntity implements ISessionSchemaPr
   @Column({ generated: 'uuid', unique: true, name: 'uuid' })
   uuid: string;
 
-  @Column({ type: 'varchar', length: 255 })
+  @Column({ type: 'varchar', length: SESSION.SESSION_ID.MAX_LENGTH })
   sessionId: string;
 
-  @Column({ type: 'varchar', length: 50 })
+  @Column({ type: 'varchar', length: SESSION.SESSION_TYPE.MAX_LENGTH })
   sessionType: string;
 
   @Column({ type: 'int' })
   sessionDuration: number;
 
-  @Column({ type: 'varchar', length: 50 })
+  @Column({ type: 'varchar', length: SESSION.SESSION_CLOSED_TYPE.MAX_LENGTH })
   sessionClosedType: string;
 
-  @Column({ type: 'text' })
+  @Column({ type: 'text', length: SESSION.TOKEN.MAX_LENGTH })
   token: string;
 
   @Column({ type: 'timestamp' })
@@ -36,22 +37,22 @@ export class SessionEntity extends TypeormBaseEntity implements ISessionSchemaPr
   @Column({ type: 'timestamp', nullable: true })
   loggedOutAt?: Date;
 
-  @Column({ type: 'varchar', length: 100 })
+  @Column({ type: 'varchar', length: SESSION.IP_ADDRESS.MAX_LENGTH })
   ipAddress: string;
 
-  @Column({ type: 'text' })
+  @Column({ type: 'text', length: SESSION.REFRESH_TOKEN.MAX_LENGTH })
   refreshToken: string;
 
-  @Column({ type: 'varchar', length: 255 })
+  @Column({ type: 'varchar', length: SESSION.USER_AGENT.MAX_LENGTH })
   userAgent: string;
 
   @Column({ type: 'int', default: 0 })
   failedAttempts: number;
 
-  @Column({ type: 'varchar', length: 100 })
+  @Column({ type: 'varchar', length: SESSION.ORIGIN.MAX_LENGTH })
   origin: string;
 
-  @Column({ type: 'varchar', length: 255 })
+  @Column({ type: 'varchar', length: SESSION.LOCATION.MAX_LENGTH })
   location: string;
 
   @ManyToOne(() => AccountEntity, (account) => account.sessions, { nullable: false })
