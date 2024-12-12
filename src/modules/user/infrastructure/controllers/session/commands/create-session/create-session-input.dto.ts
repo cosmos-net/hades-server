6d4f6d51a9210f02fe47d1827c61f1ca3e7e4020
Jesús Crespo <crespojesus9@gmail.com>
2024-12-11 21:05:49 -0800

    feat: :sparkles: session dtos with parameters and controller based on entity was created

--- src/modules/user/infrastructure/controllers/session/commands/create-session/create-session-input.dto.ts
+++ src/modules/user/infrastructure/controllers/session/commands/create-session/create-session-input.dto.ts
@@ -1,7 +1,54 @@
-import { IsString, IsNotEmpty } from 'class-validator';
+import { IsString, IsNotEmpty, IsNumber, Length } from 'class-validator';
 
 export class CreateSessionInput {
   @IsString()
   @IsNotEmpty()
+  @Length(5, 255)
+  public readonly sessionId: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Length(5, 50)
+  public readonly sessionType: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  public readonly sessionDuration: number;
+
+  @IsString()
+  @IsNotEmpty()
+  @Length(5, 50)
+  public readonly sessionClosedType: string;
+
+  @IsString()
+  @IsNotEmpty()
   public readonly token: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Length(5, 100)
+  public readonly ipAddress: string;
+
+  @IsString()
+  @IsNotEmpty()
+  public readonly refreshToken: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Length(5, 255)
+  public readonly userAgent: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  public readonly failedAttempts: number;
+
+  @IsString()
+  @IsNotEmpty()
+  @Length(5, 100)
+  public readonly origin: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Length(5, 255)
+  public readonly location: string;
 }
