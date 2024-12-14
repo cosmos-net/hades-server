cff11a9ef77f1ad0c36872fe0b52245cd25eff16
2314-RAustin <0315108515@miutt.edu.mx>
2024-12-13 18:37:15 -0800

    refactor(user): :recycle: update matching return type in IUserRepositoryContract to ListUserAggregate for improved consistency

--- src/modules/user/domain/contracts/user-repository.contract.ts
+++ src/modules/user/domain/contracts/user-repository.contract.ts
@@ -1,11 +1,11 @@
 import { Criteria } from '@common/domain/criteria/criteria';
+import { ListUserAggregate } from '@user/domain/aggregates/list-user.aggregate';
 import { UserAggregate } from '@user/domain/aggregates/user.aggregate';
-import { ListUserModel } from '@user/domain/models/user/user-list.model';
 
 export interface IUserRepositoryContract {
   persist(user: UserAggregate): Promise<UserAggregate>;
   getOneBy(UUID: string): Promise<UserAggregate>;
   destroy(UUID: string): Promise<boolean>;
   archive(UUID: string): Promise<boolean>;
-  matching(criteria: Criteria): Promise<ListUserModel>;
+  matching(criteria: Criteria): Promise<ListUserAggregate>;
 }
