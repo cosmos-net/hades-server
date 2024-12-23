19f815db42eaf633a8f25e48339e9e89fb0b12e2
oztin <roberto.austin@innovationsmi.com>
2024-12-23 14:49:50 -0800

    refactor(role-repository): change interface to abstract class and add options parameter to getOneBy method

--- src/modules/role/domain/contracts/role-repository.contract.ts
+++ src/modules/role/domain/contracts/role-repository.contract.ts
@@ -2,11 +2,11 @@ import { Criteria } from '@common/domain/criteria/criteria';
 import { ListRoleModel } from '@role/domain/models/role-list.model';
 import { RoleModel } from '@role/domain/models/role.model';
 
-export interface IRoleRepositoryContract {
-  isNameAvailable(name: string): Promise<boolean>;
-  persist(role: RoleModel): Promise<RoleModel>;
-  getOneBy(UUID: string): Promise<RoleModel>;
-  destroy(UUID: string): Promise<boolean>;
-  archive(UUID: string): Promise<boolean>;
-  matching(criteria: Criteria): Promise<ListRoleModel>;
+export abstract class IRoleRepositoryContract {
+  abstract isNameAvailable(name: string): Promise<boolean>;
+  abstract persist(role: RoleModel): Promise<RoleModel>;
+  abstract getOneBy(UUID: string, options?: {withDeleted: boolean}): Promise<RoleModel>;
+  abstract destroy(UUID: string): Promise<boolean>;
+  abstract archive(UUID: string): Promise<boolean>;
+  abstract matching(criteria: Criteria): Promise<ListRoleModel>;
 }
