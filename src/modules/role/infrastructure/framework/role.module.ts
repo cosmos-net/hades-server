c8a7a42e6561b1cebbe954bd0d58fb53b2f29ba3
oztin <roberto.austin@innovationsmi.com>
2024-12-23 14:51:02 -0800

    feat(role-module): refactor role module to use dependency injection for domain services and update imports

--- src/modules/role/infrastructure/framework/role.module.ts
+++ src/modules/role/infrastructure/framework/role.module.ts
@@ -12,7 +12,7 @@ import { CreateRoleDomainService } from '@role/domain/domain-service/create-role
 import { DestroyRoleDomainService } from '@role/domain/domain-service/destroy-role.domain-service';
 import { GetRoleDomainService } from '@role/domain/domain-service/get-role.domain-service';
 import { UpdateRoleDomainService } from '@role/domain/domain-service/update-role.domain-service';
-import { ListRoleDomainService } from '@role/domain/services/list-role.domain-service';
+import { ListRoleDomainService } from '@role/domain/domain-service/list-role.domain-service';
 import { CreateRoleController } from '@role/infrastructure/controllers/commands/create-role/create-role.controller';
 import { RoleReDescribedEventHandler } from '@role/infrastructure/events-handler/success/role-redescribed.event-handler';
 import { RoleEntity } from '@role/infrastructure/persistence/entities/role.entity';
@@ -22,6 +22,8 @@ import { DestroyRoleController } from '@role/infrastructure/controllers/commands
 import { UpdateRoleController } from '@role/infrastructure/controllers/commands/update-role/update-role.controller';
 import { GetRoleController } from '@role/infrastructure/controllers/queries/get-role/get-role.controller';
 import { ListRoleController } from '@role/infrastructure/controllers/queries/list-role/list-role.controller';
+import { IRoleRepositoryContract } from '@role/domain/contracts/role-repository.contract';
+import { DestroyRoleUseCase } from '@role/application/commands/use-cases/destroy-role/destroy-role.use-case';
 
 @Module({
   imports: [
@@ -33,21 +35,68 @@ import { ListRoleController } from '@role/infrastructure/controllers/queries/lis
     CreateRoleUseCase,
     UpdateRoleUseCase,
     ArchiveRoleUseCase,
-    DestroyRoleDomainService,
+    DestroyRoleUseCase,
     GetRoleUseCase,
     ListRoleUseCase,
     // Domain Services
-    RoleTypeormRepository,
     CreateRoleDomainService,
     UpdateRoleDomainService,
     ArchiveRoleDomainService,
+    DestroyRoleDomainService,
     GetRoleDomainService,
     ListRoleDomainService,
+    // Inversion of dependencies, Control principle (IoC) to domain services
+    {
+      provide: CreateRoleDomainService,
+      useFactory: (roleRepository: IRoleRepositoryContract): CreateRoleDomainService => {
+        return new CreateRoleDomainService(roleRepository);
+      },
+      inject: [IRoleRepositoryContract],
+    }, 
+    {
+      provide: UpdateRoleDomainService,
+      useFactory: (roleRepository: IRoleRepositoryContract): UpdateRoleDomainService => {
+        return new UpdateRoleDomainService(roleRepository);
+      },
+      inject: [IRoleRepositoryContract],
+    },
+    {
+      provide: ArchiveRoleDomainService,
+      useFactory: (roleRepository: IRoleRepositoryContract): ArchiveRoleDomainService => {
+        return new ArchiveRoleDomainService(roleRepository);
+      },
+      inject: [IRoleRepositoryContract],
+    },
+    {
+      provide: DestroyRoleDomainService,
+      useFactory: (roleRepository: IRoleRepositoryContract): DestroyRoleDomainService => {
+        return new DestroyRoleDomainService(roleRepository);
+      },
+      inject: [IRoleRepositoryContract],
+    },
+    {
+      provide: GetRoleDomainService,
+      useFactory: (roleRepository: IRoleRepositoryContract): GetRoleDomainService => {
+        return new GetRoleDomainService(roleRepository);
+      },
+      inject: [IRoleRepositoryContract],
+    },
+    {
+      provide: ListRoleDomainService,
+      useFactory: (roleRepository: IRoleRepositoryContract): ListRoleDomainService => {
+        return new ListRoleDomainService(roleRepository);
+      },
+      inject: [IRoleRepositoryContract],
+    },
     // Event Handlers
     RoleReDescribedEventHandler,
     // Repositories
     {
       provide: ROLE_REPOSITORY,
+      useClass: RoleTypeormRepository,
+    },
+    {
+      provide: IRoleRepositoryContract,
       useClass: RoleTypeormRepository,
     },
   ],
