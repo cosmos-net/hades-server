404f8670ccb5160da1404228be2df96eefeaf4bf
oztin <0315108515@miutt.edu.mx>
2024-12-23 14:47:49 -0800

    refactor(exceptions): rename handleDomainError to handleDomainException for clarity

--- src/helpers/exceptions/handle-domain-exception.helper.ts
+++ src/helpers/exceptions/handle-domain-exception.helper.ts
@@ -8,7 +8,7 @@ type StructureError = {
 
 const INTERNAL_SERVER_ERROR = 500;
 
-export default function handleDomainError(error: DomainException): StructureError {
+export default function handleDomainException(error: DomainException): StructureError {
   const exceptionType = error.constructor as typeof DomainException;
   const statusCode = exceptionMapping.get(exceptionType) || INTERNAL_SERVER_ERROR;
   const message = error.message || 'Internal server error';
