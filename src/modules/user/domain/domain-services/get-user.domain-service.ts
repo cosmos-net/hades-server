8bdb2914467b7262e677c1dd811f5f72355fe8cd
oztin <roberto.austin@innovationsmi.com>
2024-12-23 14:51:42 -0800

    refactor(user-domain-services): simplify error handling and improve code readability in domain services

--- src/modules/user/domain/domain-services/get-user.domain-service.ts
+++ src/modules/user/domain/domain-services/get-user.domain-service.ts
@@ -1,27 +1,17 @@
-import DomainException from '@common/domain/exceptions/domain.exception';
 import { UserAggregate } from '@user/domain/aggregates/user.aggregate';
 import { IUserRepositoryContract } from '@user/domain/contracts/user-repository.contract';
-import { ExceptionFactory } from '@user/domain/exceptions/exception.factory';
 import { UserNotFoundException } from '@user/domain/exceptions/user-not-found.exception';
 
 export class GetUserDomainService {
   constructor(private readonly userRepository: IUserRepositoryContract) {}
 
   async go(uuid: string): Promise<UserAggregate> {
-    try {
-      const user = await this.userRepository.getOneBy(uuid);
+    const user = await this.userRepository.getOneBy(uuid);
 
-      if (!user) {
-        throw new UserNotFoundException(`User with uuid ${uuid} not found`);
-      }
-
-      return user;
-    } catch (error) {
-      if (error instanceof DomainException) {
-        ExceptionFactory.createException(error.name, error.message);
-      }
-
-      throw error;
+    if (!user) {
+      throw new UserNotFoundException(`User with uuid ${uuid} not found`);
     }
+
+    return user;
   }
 }
--- src/modules/user/domain/domain-services/list-user.domain-service.ts
+++ src/modules/user/domain/domain-services/list-user.domain-service.ts
@@ -1,28 +1,18 @@
 import { Criteria } from '@common/domain/criteria/criteria';
-import DomainException from '@common/domain/exceptions/domain.exception';
 import { ListUserAggregate } from '@user/domain/aggregates/list-user.aggregate';
 import { IUserRepositoryContract } from '@user/domain/contracts/user-repository.contract';
-import { ExceptionFactory } from '@user/domain/exceptions/exception.factory';
 import { UserNotFoundException } from '@user/domain/exceptions/user-not-found.exception';
 
 export class ListUserDomainService {
   constructor(private readonly userRepository: IUserRepositoryContract) {}
 
   async go(criteria: Criteria): Promise<ListUserAggregate> {
-    try {
-      const listUserAggregate = await this.userRepository.matching(criteria);
+    const listUserAggregate = await this.userRepository.matching(criteria);
 
-      if (listUserAggregate.getTotal === 0) {
-        throw new UserNotFoundException('No users found');
-      }
-
-      return listUserAggregate;
-    } catch (error) {
-      if (error instanceof DomainException) {
-        ExceptionFactory.createException(error.name, error.message);
-      }
-
-      throw error;
+    if (listUserAggregate.getTotal === 0) {
+      throw new UserNotFoundException('No users found');
     }
+
+    return listUserAggregate;
   }
 }
