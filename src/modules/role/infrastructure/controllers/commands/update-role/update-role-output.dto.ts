8955d6744aaa7c3dc01f9e624064453d1e5c25de
oztin <0315108515@miutt.edu.mx>
2024-12-23 14:50:52 -0800

    feat(update-role): enhance input/output DTOs with UUID and error handling in update role functionality

--- src/modules/role/infrastructure/controllers/commands/update-role/update-role-output.dto.ts
+++ src/modules/role/infrastructure/controllers/commands/update-role/update-role-output.dto.ts
@@ -1,6 +1,15 @@
 export class UpdateRoleOutputDto {
+  public readonly id: number;
   public readonly uuid: string;
   public readonly name: string;
   public readonly description: string;
   public readonly updatedAt: Date;
+
+  constructor(root: UpdateRoleOutputDto) {
+    this.id = root.id;
+    this.uuid = root.uuid;
+    this.name = root.name;
+    this.description = root.description;
+    this.updatedAt = root.updatedAt;
+  }
 }
