8cd9f3ad10b43aae235a76df046ab130469680de
oztin <0315108515@miutt.edu.mx>
2024-12-23 14:50:19 -0800

    refactor(exceptions): remove unused exception factory and introduce role-specific exceptions

---
+++ src/modules/role/domain/exceptions/role-not-archived-exception.ts
@@ -0,0 +1,9 @@
+import BusinessConflictDomainException from "@common/domain/exceptions/business-conflict.exception";
+
+export class RoleNotArchivedException extends BusinessConflictDomainException {
+  public static readonly ROLE_NOT_ARCHIVED = 'Role not archived';
+
+  constructor(public readonly message: string) {
+    super(JSON.stringify({ name: RoleNotArchivedException.ROLE_NOT_ARCHIVED, message }));
+  }
+}
