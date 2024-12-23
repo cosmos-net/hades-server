12f73afd0c5c5baf06dfd3d506daaccaf7551757
oztin <0315108515@miutt.edu.mx>
2024-12-23 14:51:42 -0800

    refactor(user-domain-services): simplify error handling and improve code readability in domain services

--- src/modules/user/domain/domain-services/destroy-user.domain-service.ts
+++ src/modules/user/domain/domain-services/destroy-user.domain-service.ts
@@ -1,29 +1,20 @@
-import DomainException from '@common/domain/exceptions/domain.exception';
 import { UserAggregate } from '@user/domain/aggregates/user.aggregate';
 import { IUserRepositoryContract } from '@user/domain/contracts/user-repository.contract';
-import { ExceptionFactory } from '@user/domain/exceptions/exception.factory';
 import { UserNotFoundException } from '@user/domain/exceptions/user-not-found.exception';
 
 export class DestroyUserDomainService {
   constructor(private readonly userRepository: IUserRepositoryContract) {}
 
   async go(uuid: string): Promise<UserAggregate> {
-    try {
-      const userAggregate = await this.userRepository.getOneBy(uuid);
+    
+    const userAggregate = await this.userRepository.getOneBy(uuid);
 
-      if (!userAggregate) {
-        throw new UserNotFoundException(`User with uuid ${uuid} not found`);
-      }
-
-      userAggregate.destroy();
-
-      return userAggregate;
-    } catch (error) {
-      if (error instanceof DomainException) {
-        ExceptionFactory.createException(error.name, error.message);
-      }
-
-      throw error;
+    if (!userAggregate) {
+      throw new UserNotFoundException(`User with uuid ${uuid} not found`);
     }
+
+    userAggregate.destroy();
+
+    return userAggregate;
   }
 }
