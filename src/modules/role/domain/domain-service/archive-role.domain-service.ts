74b27017b63311068158cdb3a72d45096b889ebf
oztin <0315108515@miutt.edu.mx>
2024-12-23 14:49:57 -0800

    refactor(archive-role): simplify error handling and streamline role retrieval logic

--- src/modules/role/domain/domain-service/archive-role.domain-service.ts
+++ src/modules/role/domain/domain-service/archive-role.domain-service.ts
@@ -1,6 +1,4 @@
-import DomainException from '@common/domain/exceptions/domain.exception';
 import { IRoleRepositoryContract } from '@role/domain/contracts/role-repository.contract';
-import { ExceptionFactory } from '@role/domain/exceptions/exception.factory';
 import { RoleNotFoundException } from '@role/domain/exceptions/role-not-found.exception';
 import { RoleModel } from '@role/domain/models/role.model';
 
@@ -8,20 +6,14 @@ export class ArchiveRoleDomainService {
   constructor(private readonly roleRepository: IRoleRepositoryContract) {}
 
   async go(uuid: string): Promise<RoleModel> {
-    try {
-      const roleModel = await this.roleRepository.getOneBy(uuid);
+    const roleModel = await this.roleRepository.getOneBy(uuid);
 
-      if (!roleModel) {
-        throw new RoleNotFoundException(`Role with uuid ${uuid} not found`);
-      }
-
-      roleModel.archive(roleModel.uuid, roleModel.name);
-
-      return roleModel;
-    } catch (error) {
-      if (error instanceof DomainException) {
-        ExceptionFactory.createException(error.name, error.message);
-      }
+    if (!roleModel) {
+      throw new RoleNotFoundException(`Role with uuid ${uuid} not found`);
     }
+
+    roleModel.archive(roleModel.uuid, roleModel.name);
+
+    return roleModel;
   }
 }
