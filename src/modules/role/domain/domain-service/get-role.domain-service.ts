741a5e469f62ecf761da867b4a2abb0e5d76625a
oztin <roberto.austin@innovationsmi.com>
2024-12-23 14:50:06 -0800

    refactor(get-role): simplify role retrieval and remove unnecessary error handling

--- src/modules/role/domain/domain-service/get-role.domain-service.ts
+++ src/modules/role/domain/domain-service/get-role.domain-service.ts
@@ -1,26 +1,16 @@
-import DomainException from '@common/domain/exceptions/domain.exception';
 import { IRoleRepositoryContract } from '@role/domain/contracts/role-repository.contract';
-import { ExceptionFactory } from '@role/domain/exceptions/exception.factory';
 import { RoleNotFoundException } from '@role/domain/exceptions/role-not-found.exception';
 
 export class GetRoleDomainService {
   constructor(private readonly roleRepository: IRoleRepositoryContract) {}
 
   async go(uuid: string) {
-    try {
-      const role = await this.roleRepository.getOneBy(uuid);
+    const role = await this.roleRepository.getOneBy(uuid);
 
-      if (!role) {
-        throw new RoleNotFoundException(`Role with uuid ${uuid} not found`);
-      }
-
-      return role;
-    } catch (error) {
-      if (error instanceof DomainException) {
-        ExceptionFactory.createException(error.name, error.message);
-      }
-
-      throw error;
+    if (!role) {
+      throw new RoleNotFoundException(`Role with uuid ${uuid} not found`);
     }
+
+    return role;
   }
 }
