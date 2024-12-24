9d9a75dc27961b0d93b8ec16d642957595b7f5d0
oztin <0315108515@miutt.edu.mx>
2024-12-24 01:58:39 -0800

    refactor(output-pagination.dto.ts): initialize items and metadata properties in PaginationOutputDto

--- src/modules/common/infrastructure/dtos/pagination-options/output-pagination.dto.ts
+++ src/modules/common/infrastructure/dtos/pagination-options/output-pagination.dto.ts
@@ -8,11 +8,11 @@ interface IPaginationMetadataDto {
 }
 
 export class PaginationOutputDto<T> {
-  readonly items: T[];
-
-  readonly metadata: IPaginationMetadataDto;
+  readonly items: T[] = [];
+  readonly metadata: IPaginationMetadataDto = {} as IPaginationMetadataDto;
 
   constructor(items: T[], page: number, limit: number, total: number) {
+    this.items = items;
     this.metadata.page = page;
     this.metadata.limit = limit;
     this.metadata.total = total;
