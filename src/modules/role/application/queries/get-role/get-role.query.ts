2947752b77b4cebba88313ceecc1d2fcbdcfb060
oztin <0315108515@miutt.edu.mx>
2024-12-26 14:17:40 -0800

    feat: :sparkles: add withArchived option to GetRole query and update related services for role retrieval

--- src/modules/role/application/queries/get-role/get-role.query.ts
+++ src/modules/role/application/queries/get-role/get-role.query.ts
@@ -3,7 +3,10 @@ import { IQuery } from '@nestjs/cqrs';
 export class GetRoleQuery implements IQuery {
   public readonly uuid: string;
 
+  public readonly withArchived?: boolean;
+
   constructor(root: GetRoleQuery) {
     this.uuid = root.uuid;
+    this.withArchived = root.withArchived;
   }
 }
--- src/modules/role/domain/domain-service/get-role.domain-service.ts
+++ src/modules/role/domain/domain-service/get-role.domain-service.ts
@@ -4,8 +4,8 @@ import { RoleNotFoundException } from '@role/domain/exceptions/role-not-found.ex
 export class GetRoleDomainService {
   constructor(private readonly roleRepository: IRoleRepositoryContract) {}
 
-  async go(uuid: string) {
-    const role = await this.roleRepository.getOneBy(uuid);
+  async go(uuid: string, withArchived?: boolean) {
+    const role = await this.roleRepository.getOneBy(uuid, { withArchived });
 
     if (!role) {
       throw new RoleNotFoundException(`Role with uuid ${uuid} not found`);
