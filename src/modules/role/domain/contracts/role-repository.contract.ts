f0654164a03b6385d1d58a1ccf6bfd3b874b1539
oztin <roberto.austin@innovationsmi.com>
2024-12-26 13:49:44 -0800

    feat: :sparkles: update IRoleRepositoryContract to use IOptions for getOneBy method

--- src/modules/role/domain/contracts/role-repository.contract.ts
+++ src/modules/role/domain/contracts/role-repository.contract.ts
@@ -2,10 +2,13 @@ import { Criteria } from '@common/domain/criteria/criteria';
 import { ListRoleModel } from '@role/domain/models/role-list.model';
 import { RoleModel } from '@role/domain/models/role.model';
 
+export interface IOptions {
+  withDeleted: boolean;
+}
+
 export abstract class IRoleRepositoryContract {
-  abstract isNameAvailable(name: string): Promise<boolean>;
   abstract persist(role: RoleModel): Promise<RoleModel>;
-  abstract getOneBy(UUID: string, options?: {withDeleted: boolean}): Promise<RoleModel>;
+  abstract getOneBy(UUID: string, options?: IOptions): Promise<RoleModel>;
   abstract destroy(UUID: string): Promise<boolean>;
   abstract archive(UUID: string): Promise<boolean>;
   abstract matching(criteria: Criteria): Promise<ListRoleModel>;
