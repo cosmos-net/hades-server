5741fddb7dfdc167ec92403126bd5003da2d4432
Jesús Crespo <crespojesus9@gmail.com>
2024-12-27 10:39:55 -0800

    fix: fixing merge conflict from coding pairing into files like main branch

--- src/modules/user/infrastructure/controllers/queries/get-user/get-user-output.dto.ts
+++ src/modules/user/infrastructure/controllers/queries/get-user/get-user-output.dto.ts
@@ -1,27 +1,42 @@
 import { ProfileGenderEnum } from '@user/domain/constants/general-rules';
 
+interface IAddress {
+  street: string;
+  extNumber: string;
+  intNumber?: string | null;
+  neighborhood: string;
+  zipCode: string;
+  city: string;
+  state: string;
+  country: string;
+}
+
+interface IProfile {
+  name: string;
+  lastName: string;
+  secondLastName: string;
+  phoneNumber: string;
+  gender: ProfileGenderEnum;
+  address: IAddress;
+}
+
+interface IAccount {
+  username: string;
+  email: string;
+}
+
+interface ICreateUserOutputDto {
+  id: string;
+  uuid: string;
+  status: string;
+  accounts: IAccount[];
+  profile: IProfile;
+}
+
 export class GetUserOutputDto {
-  public readonly user: {
-    id: string;
-    uuid: string;
-    status: string;
-    account: {
-      username: string;
-      email: string;
-    };
-    profile: {
-      name: string;
-      lastName: string;
-      secondLastName: string;
-      phoneNumber: string;
-      gender: ProfileGenderEnum;
-      address: {
-        street: string;
-        city: string;
-        state: string;
-        country: string;
-        zipCode: string;
-      };
-    };
-  };
+  public readonly user: ICreateUserOutputDto;
+
+  constructor(root: GetUserOutputDto) {
+    this.user = root.user;
+  }
 }
