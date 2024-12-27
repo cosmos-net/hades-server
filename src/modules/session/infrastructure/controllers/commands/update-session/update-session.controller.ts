7aff4ba5287781cb6d630feead8dd96b0fccfb48
Jesús Crespo <crespojesus9@gmail.com>
2024-12-27 12:56:33 -0800

    feat: :sparkles: update-session input, output and controller was created with logic

---
+++ src/modules/session/infrastructure/controllers/commands/update-session/update-session.controller.ts
@@ -0,0 +1,26 @@
+import { Controller } from '@nestjs/common';
+import { CommandBus } from '@nestjs/cqrs';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+
+import { CMDS_HADES } from '@common/infrastructure/controllers/constants';
+import { UpdateSessionCommand } from '@session/application/commands/use-cases/update-session/update-session.command';
+import { UpdateSessionInputDto } from '@session/infrastructure/controllers/commands/update-session/update-session.input.dto';
+import { UpdateSessionOutputDto } from '@session/infrastructure/controllers/commands/update-session/update-session.output.dto';
+
+@Controller()
+export class UpdateSessionController {
+  constructor(private readonly commandBus: CommandBus) {}
+
+  @MessagePattern({ cmd: CMDS_HADES.SESSION.UPDATE })
+  async update(@Payload() updateSessionInputDto: UpdateSessionInputDto): Promise<UpdateSessionOutputDto> {
+    return this.commandBus.execute(
+      new UpdateSessionCommand({
+        uuid: updateSessionInputDto.uuid,
+        sessionClosedType: updateSessionInputDto.sessionClosedType,
+        loggedOutAt: updateSessionInputDto.loggedOutAt,
+        refreshToken: updateSessionInputDto.refreshToken,
+        failedAttempts: updateSessionInputDto.failedAttempts,
+      }),
+    );
+  }
+}
