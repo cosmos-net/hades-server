5f5b65beba300bb625101fd8c0a1d8026f7e19eb
Jesús Crespo <crespojesus9@gmail.com>
2024-12-27 14:35:47 -0800

    feat: :sparkles: get session controller was created with logic

---
+++ src/modules/session/infrastructure/controllers/queries/get-session/get-session-input.dto.ts
@@ -0,0 +1,17 @@
+import { IsBoolean, IsOptional, IsUUID } from 'class-validator';
+import { Transform } from 'class-transformer';
+
+export class GetSessionInputDto {
+  @IsUUID()
+  public readonly uuid: string;
+
+  @IsOptional()
+  @Transform(({ value }) => {
+    if (typeof value === 'string') {
+      return value.toLowerCase() === 'true';
+    }
+    return Boolean(value);
+  })
+  @IsBoolean()
+  public readonly withArchived?: boolean = false;
+}
---
+++ src/modules/session/infrastructure/controllers/queries/get-session/get-session.controller.ts
@@ -0,0 +1,25 @@
+import { Controller } from '@nestjs/common';
+import { QueryBus } from '@nestjs/cqrs';
+import { MessagePattern, Payload, RpcException } from '@nestjs/microservices';
+
+import { CMDS_HADES } from '@common/infrastructure/controllers/constants';
+import { GetSessionInputDto } from '@session/infrastructure/controllers/queries/get-session/get-session-input.dto';
+import { GetSessionOutputDto } from '@session/infrastructure/controllers/queries/get-session/get-session-output.dto';
+import { GetSessionQuery } from '@session/application/queries/get-session/get-session.query';
+import { SessionModel } from '@session/domain/models/session.model';
+
+@Controller()
+export class GetSessionController {
+  constructor(private readonly queryBus: QueryBus) {}
+
+  @MessagePattern({ cmd: CMDS_HADES.SESSION.GET })
+  async get(@Payload() getSessionInputDto: GetSessionInputDto): Promise<GetSessionOutputDto> {
+    try {
+    const result = await this.queryBus.execute<GetSessionQuery, SessionModel>(new GetSessionQuery(getSessionInputDto));
+
+    return new GetSessionOutputDto(result);
+    } catch (error: any) {
+      throw new RpcException(error);
+    }
+  }
+}
