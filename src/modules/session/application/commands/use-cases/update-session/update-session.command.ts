4a289cb9bb40860095cbf5a277c791eb5df67f54
Jesús Crespo <crespojesus9@gmail.com>
2024-12-27 12:57:06 -0800

    feat: :sparkles: update session command from application layer was created with doc parameters followed

---
+++ src/modules/session/application/commands/use-cases/update-session/update-session.command.ts
@@ -0,0 +1,17 @@
+import { ICommand } from '@nestjs/cqrs';
+
+export class UpdateSessionCommand implements ICommand {
+  public readonly uuid: string;
+  public readonly sessionClosedType: string;
+  public readonly loggedOutAt: Date;
+  public readonly refreshToken: string;
+  public readonly failedAttempts: number;
+  
+  constructor(root: UpdateSessionCommand) {
+    this.uuid = root.uuid;
+    this.sessionClosedType = root.sessionClosedType;
+    this.loggedOutAt = root.loggedOutAt;
+    this.refreshToken = root.refreshToken;
+    this.failedAttempts = root.failedAttempts;
+  }
+}
