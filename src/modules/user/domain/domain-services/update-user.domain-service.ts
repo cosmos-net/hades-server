11d7f29314750c6787a20aa0cc31dfede7c39096
oztin <0315108515@miutt.edu.mx>
2024-12-28 14:22:36 -0800

    feat: refactor update user use case to simplify parameters and add user not found exception handling

--- src/modules/user/domain/domain-services/update-user.domain-service.ts
+++ src/modules/user/domain/domain-services/update-user.domain-service.ts
@@ -2,20 +2,17 @@ import { UserAggregate } from '@user/domain/aggregates/user.aggregate';
 import { IUserRepositoryContract } from '@user/domain/contracts/user-repository.contract';
 import { DeepPartialExceptAccountBaseSchema } from '@user/domain/schemas/account/account.schema-primitive';
 import { DeepPartialProfileBaseSchema } from '@user/domain/schemas/profile/profile.schema-primitive';
+import { UserNotFoundException } from '@user/domain/exceptions/user/user-not-found.exception';
 
 export class UpdateUserDomainService {
   constructor(private readonly userRepository: IUserRepositoryContract) {}
 
-  async go({
-    uuid,
-    accounts,
-    profile,
-  }: {
-    uuid: string;
-    accounts: DeepPartialExceptAccountBaseSchema;
-    profile: DeepPartialProfileBaseSchema;
-  }): Promise<UserAggregate> {
+  async go(uuid: string, accounts: DeepPartialExceptAccountBaseSchema, profile: DeepPartialProfileBaseSchema): Promise<UserAggregate> {
     const userAggregate = await this.userRepository.getOneBy(uuid);
+
+    if (!userAggregate) {
+      throw new UserNotFoundException(`User with uuid ${uuid} not found`);
+    }
 
     userAggregate.update(accounts, profile);
 
