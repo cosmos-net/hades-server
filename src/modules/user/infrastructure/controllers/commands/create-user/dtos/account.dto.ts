8e6bf3d734b86865bc781534d92d89007106b521
oztin <roberto.austin@innovationsmi.com>
2024-12-28 13:29:03 -0800

    feat: remove TODO comments for password confirmation handling in AccountDTO

--- src/modules/user/infrastructure/controllers/commands/create-user/dtos/account.dto.ts
+++ src/modules/user/infrastructure/controllers/commands/create-user/dtos/account.dto.ts
@@ -20,14 +20,12 @@ export class AccountDTO {
   @IsDefined()
   public readonly email: string;
 
-  //TODO: handler confirmation of password
   @IsString()
   @IsNotEmpty()
   @Length(MIN_ACCOUNT_PASSWORD_LENGTH, MAX_ACCOUNT_PASSWORD_LENGTH)
   @IsDefined()
   public readonly password: string;
 
-  //TODO: handler confirmation of password
   @IsString()
   @IsNotEmpty()
   @Length(MIN_ACCOUNT_PASSWORD_LENGTH, MAX_ACCOUNT_PASSWORD_LENGTH)
