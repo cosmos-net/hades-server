30cd48bc56d7aeba6eb8eef31df933274e72a428
oztin <roberto.austin@innovationsmi.com>
2024-12-28 13:22:41 -0800

    feat: add user status to CreateUserCommand and implement AccountConfirmationPasswordException

---
+++ src/modules/user/domain/exceptions/account/account-confirmation-password.exception.ts
@@ -0,0 +1,14 @@
+import BusinessConflictDomainException from "@common/domain/exceptions/business-conflict.exception";
+
+export class AccountConfirmationPasswordException extends BusinessConflictDomainException {
+  public static readonly ACCOUNT_CONFIRMATION_PASSWORD = 'ACCOUNT_CONFIRMATION_PASSWORD';
+
+  constructor(public readonly message: string) {
+    super(
+      JSON.stringify({
+        name: AccountConfirmationPasswordException.ACCOUNT_CONFIRMATION_PASSWORD,
+        message,
+      }),
+    );
+  }
+}
