76e11114b6a3efe83507cb944434d96ddbec4764
oztin <roberto.austin@innovationsmi.com>
2024-12-28 13:27:56 -0800

    feat: update UserCreatedEvent to use models for user, accounts, and profile

--- src/modules/user/domain/events/events-success-domain/user-created.event.ts
+++ src/modules/user/domain/events/events-success-domain/user-created.event.ts
@@ -1,6 +1,11 @@
+import { AccountModel } from "@user/domain/models/account/account.model";
+import { ProfileModel } from "@user/domain/models/profile/profile.model";
+import { UserModel } from "@user/domain/models/user/user.model";
+
 export class UserCreatedEvent {
   constructor(
-    public readonly uuid: string,
-    public readonly status: string,
+    public readonly userModel: UserModel,
+    public readonly AccountsModel: AccountModel[],
+    public readonly profileModel: ProfileModel
   ) {}
 }
