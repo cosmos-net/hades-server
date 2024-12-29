bef3459f426b587075d43b389ec5af8820705366
oztin <roberto.austin@innovationsmi.com>
2024-12-29 02:13:14 -0800

    feat: update UserArchivedEvent to use models for user, accounts, and profile

--- src/modules/user/domain/events/events-success-domain/user-archived.event.ts
+++ src/modules/user/domain/events/events-success-domain/user-archived.event.ts
@@ -1,6 +1,11 @@
+import { AccountModel } from '@user/domain/models/account/account.model';
+import { ProfileModel } from '@user/domain/models/profile/profile.model';
+import { UserModel } from '@user/domain/models/user/user.model';
+
 export class UserArchivedEvent {
   constructor(
-    public readonly uuid: string,
-    public readonly archivedAt: Date,
+    public readonly userModel: UserModel,
+    public readonly accountsModel: AccountModel[],
+    public readonly profileModel: ProfileModel,
   ) {}
 }
