83ba2a3be2d260d191e5f2b3b4b81ddd549401a9
oztin <roberto.austin@innovationsmi.com>
2024-12-29 01:25:18 -0800

    feat: enhance user update service with account and profile validation checks

--- src/modules/user/domain/domain-services/update-user.domain-service.ts
+++ src/modules/user/domain/domain-services/update-user.domain-service.ts
@@ -1,18 +1,31 @@
 import { UserAggregate } from '@user/domain/aggregates/user.aggregate';
+import { IAccountRepositoryContract } from '@user/domain/contracts/account-repository.contract';
+import { IProfileRepositoryContract } from '@user/domain/contracts/profile-repository.contract';
 import { IUserRepositoryContract } from '@user/domain/contracts/user-repository.contract';
+import { UserIsNotAvailableException } from '@user/domain/exceptions/user/user-is-not-available.exception';
+import { UserNotFoundException } from '@user/domain/exceptions/user/user-not-found.exception';
+import { AccountModel } from '@user/domain/models/account/account.model';
+import { ProfileModel } from '@user/domain/models/profile/profile.model';
 import { DeepPartialExceptAccountBaseSchema } from '@user/domain/schemas/account/account.schema-primitive';
 import { DeepPartialProfileBaseSchema } from '@user/domain/schemas/profile/profile.schema-primitive';
-import { UserNotFoundException } from '@user/domain/exceptions/user/user-not-found.exception';
-import { AccountModel } from '../models/account/account.model';
 
 export class UpdateUserDomainService {
-  constructor(private readonly userRepository: IUserRepositoryContract) {}
+  constructor(
+    private readonly userRepository: IUserRepositoryContract,
+    private readonly accountRepository: IAccountRepositoryContract,
+    private readonly profileRepository: IProfileRepositoryContract,
+  ) {}
 
-  private getAccountsNotFound(accountsWithUpdates: DeepPartialExceptAccountBaseSchema, accountsModel: AccountModel[]): DeepPartialExceptAccountBaseSchema {
-    const accountsNotFound: DeepPartialExceptAccountBaseSchema = [];  
+  private getAccountsNotFound(
+    accountsWithUpdates: DeepPartialExceptAccountBaseSchema,
+    accountsModel: AccountModel[],
+  ): DeepPartialExceptAccountBaseSchema {
+    const accountsNotFound: DeepPartialExceptAccountBaseSchema = [];
 
-    accountsWithUpdates.forEach((account) => {
-      const accountIndex = accountsModel.findIndex((accountModel) => accountModel.uuid === account.uuid);
+    accountsWithUpdates.forEach((account): void => {
+      const accountIndex = accountsModel.findIndex(
+        (accountModel): boolean => accountModel.uuid === account.uuid,
+      );
 
       if (accountIndex === -1) {
         accountsNotFound.push(account);
@@ -22,22 +35,72 @@ export class UpdateUserDomainService {
     return accountsNotFound;
   }
 
-  public async go(uuid: string, accounts: DeepPartialExceptAccountBaseSchema, profile: DeepPartialProfileBaseSchema): Promise<UserAggregate> {
+  private async validateUniqueUser(
+    accounts: DeepPartialExceptAccountBaseSchema,
+    profile: DeepPartialProfileBaseSchema,
+    accountModels: AccountModel[],
+    profileModel: ProfileModel,
+  ): Promise<void> {
+    for await (const account of accounts) {
+      const accountMatchUsername = await this.accountRepository.getOneBy(account.username);
+      const accountMatchEmail = await this.accountRepository.getOneBy(account.email);
+
+      if (accountMatchUsername) {
+        const isSameAccount = accountModels.some(
+          (account): boolean => account.uuid === accountMatchUsername.uuid,
+        );
+
+        if (!isSameAccount) {
+          throw new UserIsNotAvailableException(`Username ${account.username} is not available`);
+        }
+      }
+
+      if (accountMatchEmail) {
+        const isSameAccount = accountModels.some(
+          (account): boolean => account.uuid === accountMatchEmail.uuid,
+        );
+
+        if (!isSameAccount) {
+          throw new UserIsNotAvailableException(`Email ${account.email} is not available`);
+        }
+      }
+    }
+
+    const profileMatchPhoneNumber = await this.profileRepository.getOneBy(profile.phoneNumber);
+
+    if (profileMatchPhoneNumber) {
+      const isSameProfile = profileMatchPhoneNumber.uuid === profileModel.uuid;
+
+      if (!isSameProfile) {
+        throw new UserIsNotAvailableException(`Phone ${profile.phoneNumber} is not available`);
+      }
+    }
+  }
+
+  public async go(
+    uuid: string,
+    accounts: DeepPartialExceptAccountBaseSchema,
+    profile: DeepPartialProfileBaseSchema,
+  ): Promise<UserAggregate> {
     const userAggregate = await this.userRepository.getOneBy(uuid);
 
     if (!userAggregate) {
       throw new UserNotFoundException(`User with uuid ${uuid} not found`);
     }
 
-    const accountsNotFound = this.getAccountsNotFound(accounts, userAggregate.accountsModel);
+    const { accountsModel, profileModel } = userAggregate;
+
+    const accountsNotFound = this.getAccountsNotFound(accounts, accountsModel);
 
     if (accountsNotFound.length > 0) {
-      throw new UserNotFoundException(`Accounts with uuids ${accountsNotFound.map((account) => account.uuid).join(', ')} not found`);
+      throw new UserNotFoundException(
+        `Accounts with uuids ${accountsNotFound.map((account): string => account.uuid).join(', ')} not found`,
+      );
     }
 
-    userAggregate.update(accounts, profile);
+    await this.validateUniqueUser(accounts, profile, accountsModel, profileModel);
 
-    await this.userRepository.persist(userAggregate);
+    userAggregate.update(accounts, profile);
 
     return userAggregate;
   }
