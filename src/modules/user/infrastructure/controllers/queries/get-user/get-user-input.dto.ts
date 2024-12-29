26eb1e5251e2f1ad519714db32672f9cf2aae691
oztin <roberto.austin@innovationsmi.com>
2024-12-29 02:21:12 -0800

    feat: enhance GetUser functionality to include archived users and improve DTOs

--- src/modules/user/infrastructure/controllers/queries/get-user/get-user-input.dto.ts
+++ src/modules/user/infrastructure/controllers/queries/get-user/get-user-input.dto.ts
@@ -1,6 +1,17 @@
-import { IsUUID } from 'class-validator';
+import { Transform } from 'class-transformer';
+import { IsBoolean, IsOptional, IsUUID } from 'class-validator';
 
 export class GetUserInputDto {
   @IsUUID()
   public readonly uuid: string;
+
+  @IsOptional()
+  @Transform(({ value }): boolean => {
+    if (typeof value === 'string') {
+      return value.toLowerCase() === 'true';
+    }
+    return Boolean(value);
+  })
+  @IsBoolean()
+  public readonly withArchived: boolean = false;
 }
--- src/modules/user/infrastructure/controllers/queries/get-user/get-user.controller.ts
+++ src/modules/user/infrastructure/controllers/queries/get-user/get-user.controller.ts
@@ -4,9 +4,9 @@ import { MessagePattern, Payload, RpcException } from '@nestjs/microservices';
 
 import { CMDS_HADES } from '@common/infrastructure/controllers/constants';
 import { GetUserQuery } from '@user/application/queries/use-cases/get-user/get-user.query';
+import { UserAggregate } from '@user/domain/aggregates/user.aggregate';
 import { GetUserInputDto } from '@user/infrastructure/controllers/queries/get-user/get-user-input.dto';
 import { GetUserOutputDto } from '@user/infrastructure/controllers/queries/get-user/get-user-output.dto';
-import { UserAggregate } from '@user/domain/aggregates/user.aggregate';
 
 @Controller()
 export class GetUserController {
@@ -15,12 +15,13 @@ export class GetUserController {
   @MessagePattern({ cmd: CMDS_HADES.USER.GET })
   async get(@Payload() getUserInputDto: GetUserInputDto): Promise<GetUserOutputDto> {
     try {
-      const result = await this.queryBus.execute<GetUserQuery, UserAggregate>(new GetUserQuery(getUserInputDto));
+      const result = await this.queryBus.execute<GetUserQuery, UserAggregate>(
+        new GetUserQuery(getUserInputDto),
+      );
 
       return new GetUserOutputDto(result);
-    } catch (error: any) {
-      throw new RpcException(error);
+    } catch (error: unknown) {
+      throw new RpcException(error as Error);
     }
-    
   }
 }
