8ec49aa8b6825bd0de93545a76efdb3ce02cd355
oztin <0315108515@miutt.edu.mx>
2024-12-29 20:15:42 -0800

    feat: enhance TypeormRepository with query builder support and criteria conversion

--- src/modules/common/infrastructure/persistence/typeorm/repositories/typeorm-repository.ts
+++ src/modules/common/infrastructure/persistence/typeorm/repositories/typeorm-repository.ts
@@ -1,12 +1,28 @@
-import { Document, FindManyOptions } from 'typeorm';
+import { FindManyOptions, SelectQueryBuilder } from 'typeorm';
 
 import { Criteria } from '@common/domain/criteria/criteria';
 import { TypeormCriteriaConverter } from '@common/infrastructure/persistence/typeorm/criteria/typeorm-criteria-converter';
+import { TypeormQueryBuilderCriteriaConverter } from '@common/infrastructure/persistence/typeorm/criteria/typeorm-query-builder-criteria-converter';
 
-export abstract class TypeormRepository<T extends Document> extends TypeormCriteriaConverter<T> {
+export abstract class TypeormRepository<T> {
+  private readonly typeormCriteriaConverter = new TypeormCriteriaConverter<T>();
+  private readonly typeormBuilderCriteriaConverter = new TypeormQueryBuilderCriteriaConverter<T>();
+
   protected getQueryByCriteria(criteria: Criteria): FindManyOptions<T> {
-    const query = this.convert(criteria);
+    const query = this.typeormCriteriaConverter.convert(criteria);
 
     return query;
+  }
+
+  protected getQueryBuilderByCriteria(
+    criteria: Criteria,
+    selectQueryBuilderParam: SelectQueryBuilder<T>,
+  ): SelectQueryBuilder<T> {
+    const selectQueryBuilded = this.typeormBuilderCriteriaConverter.convert(
+      selectQueryBuilderParam,
+      criteria,
+    );
+
+    return selectQueryBuilded;
   }
 }
