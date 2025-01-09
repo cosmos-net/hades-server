105d34f50bf5e2800fa07f413efb4d0f91c7c0e2
oztin <0315108515@miutt.edu.mx>
2025-01-09 15:58:11 -0800

    refactor: :recycle: update ListRoleDomainService to return ListRoleModel and improve method signature

--- src/modules/role/domain/domain-service/list-role.domain-service.ts
+++ src/modules/role/domain/domain-service/list-role.domain-service.ts
@@ -1,11 +1,12 @@
 import { Criteria } from '@common/domain/criteria/criteria';
 import { IRoleRepositoryContract } from '@role/domain/contracts/role-repository.contract';
 import { RoleNotFoundException } from '@role/domain/exceptions/role-not-found.exception';
+import { ListRoleModel } from '@role/domain/models/role-list.model';
 
 export class ListRoleDomainService {
   constructor(private readonly roleRepository: IRoleRepositoryContract) {}
 
-  async go(criteria: Criteria) {
+  async go(criteria: Criteria): Promise<ListRoleModel> {
     const roles = await this.roleRepository.matching(criteria);
 
     if (roles.getTotal === 0) {
