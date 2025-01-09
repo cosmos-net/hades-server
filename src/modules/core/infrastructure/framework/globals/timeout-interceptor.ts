1a47d99bf66295d1702c88025042ace133ed5e4a
oztin <0315108515@miutt.edu.mx>
2025-01-09 15:58:07 -0800

    refactor: :recycle: update method signatures for clarity and improve validation logic in domain services

--- src/modules/core/infrastructure/framework/globals/timeout-interceptor.ts
+++ src/modules/core/infrastructure/framework/globals/timeout-interceptor.ts
@@ -4,7 +4,7 @@ import { timeout } from 'rxjs/operators';
 
 @Injectable()
 export class TimeOutInterceptor implements NestInterceptor {
-  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<void> {
     // TODO: Change this magic code to use the environment variable
     return next.handle().pipe(timeout(120000));
   }
