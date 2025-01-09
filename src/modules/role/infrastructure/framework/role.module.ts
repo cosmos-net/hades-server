fa9704528176a0861a3a2f6771840b5259859349
oztin <roberto.austin@innovationsmi.com>
2025-01-09 15:57:02 -0800

    feat: :sparkles: add DestroyRoleUseCase and related controllers to role module

--- src/modules/role/infrastructure/framework/role.module.ts
+++ src/modules/role/infrastructure/framework/role.module.ts
@@ -1,29 +1,30 @@
 import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
 import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { ArchiveRoleUseCase } from '@role/application/commands/use-cases/archive-role/archive-role.use-case';
 import { CreateRoleUseCase } from '@role/application/commands/use-cases/create-role/create-role.use-case';
+import { DestroyRoleUseCase } from '@role/application/commands/use-cases/destroy-role/destroy-role.use-case';
 import { UpdateRoleUseCase } from '@role/application/commands/use-cases/update-role/update-role.use-case';
 import { GetRoleUseCase } from '@role/application/queries/get-role/get-role.use-case';
 import { ListRoleUseCase } from '@role/application/queries/list-role/list-role.use-case';
 import { ROLE_REPOSITORY } from '@role/domain/constants/injection-tokens';
+import { IRoleRepositoryContract } from '@role/domain/contracts/role-repository.contract';
 import { ArchiveRoleDomainService } from '@role/domain/domain-service/archive-role.domain-service';
 import { CreateRoleDomainService } from '@role/domain/domain-service/create-role.domain-service';
 import { DestroyRoleDomainService } from '@role/domain/domain-service/destroy-role.domain-service';
 import { GetRoleDomainService } from '@role/domain/domain-service/get-role.domain-service';
-import { UpdateRoleDomainService } from '@role/domain/domain-service/update-role.domain-service';
 import { ListRoleDomainService } from '@role/domain/domain-service/list-role.domain-service';
-import { CreateRoleController } from '@role/infrastructure/controllers/commands/create-role/create-role.controller';
-import { RoleReDescribedEventHandler } from '@role/infrastructure/events-handler/success/role-redescribed.event-handler';
-import { RoleEntity } from '@role/infrastructure/persistence/typeorm/entities/role.entity';
-import { RoleTypeormRepository } from '@role/infrastructure/persistence/typeorm/repositories/role-typeorm.repository';
+import { UpdateRoleDomainService } from '@role/domain/domain-service/update-role.domain-service';
 import { ArchiveRoleController } from '@role/infrastructure/controllers/commands/archive-role/archive-role.controller';
+import { CreateRoleController } from '@role/infrastructure/controllers/commands/create-role/create-role.controller';
 import { DestroyRoleController } from '@role/infrastructure/controllers/commands/destroy-role/destroy-role.controller';
 import { UpdateRoleController } from '@role/infrastructure/controllers/commands/update-role/update-role.controller';
 import { GetRoleController } from '@role/infrastructure/controllers/queries/get-role/get-role.controller';
 import { ListRoleController } from '@role/infrastructure/controllers/queries/list-role/list-role.controller';
-import { IRoleRepositoryContract } from '@role/domain/contracts/role-repository.contract';
-import { DestroyRoleUseCase } from '@role/application/commands/use-cases/destroy-role/destroy-role.use-case';
+import { RoleReDescribedEventHandler } from '@role/infrastructure/events-handler/success/role-redescribed.event-handler';
+import { RoleEntity } from '@role/infrastructure/persistence/typeorm/entities/role.entity';
+import { RoleTypeormRepository } from '@role/infrastructure/persistence/typeorm/repositories/role-typeorm.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([RoleEntity]), CqrsModule],
