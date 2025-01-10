3b4b71fdc72215d8ff9042e68ea8d6d528bebbcf
oztin <roberto.austin@innovationsmi.com>
2025-01-09 22:06:54 -0800

    feat: :sparkles: add ActivateInvalidSession use case and controller to session module

--- src/modules/session/infrastructure/framework/session.module.ts
+++ src/modules/session/infrastructure/framework/session.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { ActiveInvalidSessionUseCase } from '@session/application/commands/use-cases/activate-invalid-session/activate-invalid-session.use-case';
 import { CreateSessionUseCase } from '@session/application/commands/use-cases/create-session/create-session.use-case';
 import { SESSION_REPOSITORY } from '@session/domain/constants/injection-tokens';
+import { ActivateInvalidSessionDomainService } from '@session/domain/domain-services/activate-invalid-session.domain-service';
 import { CreateSessionDomainService } from '@session/domain/domain-services/create-session.domain-service';
+import { ActivateInvalidSessionController } from '@session/infrastructure/controllers/commands/activate-invalid-session/activate-invalid-session.controller';
 import { CreateSessionController } from '@session/infrastructure/controllers/commands/create-session/create-session.controller';
 import { SessionEntity } from '@session/infrastructure/persistence/typeorm/entities/session.entity';
 import { SessionTypeormRepository } from '@session/infrastructure/persistence/typeorm/repositories/session-typeorm.repository';
@@ -17,6 +20,7 @@ import { AccountTypeormRepository } from '@user/infrastructure/persistence/typeo
   providers: [
     // UseCases
     CreateSessionUseCase,
+    ActiveInvalidSessionUseCase,
     // UpdateSessionUseCase,
     // ArchiveSessionUseCase,
     // DestroySessionUseCase,
@@ -25,6 +29,7 @@ import { AccountTypeormRepository } from '@user/infrastructure/persistence/typeo
 
     // Domain Services
     CreateSessionDomainService,
+    ActivateInvalidSessionDomainService,
     // UpdateSessionDomainService,
     // ArchiveSessionDomainService,
     // DestroySessionDomainService,
@@ -38,10 +43,18 @@ import { AccountTypeormRepository } from '@user/infrastructure/persistence/typeo
       inject: [ACCOUNT_REPOSITORY],
     },
     {
+      provide: ActivateInvalidSessionDomainService,
+      useFactory: (
+        sessionRepository: SessionTypeormRepository,
+      ): ActivateInvalidSessionDomainService =>
+        new ActivateInvalidSessionDomainService(sessionRepository),
+      inject: [SESSION_REPOSITORY],
+    },
+    {
       provide: SESSION_REPOSITORY,
       useClass: SessionTypeormRepository,
     },
   ],
-  controllers: [CreateSessionController],
+  controllers: [CreateSessionController, ActivateInvalidSessionController],
 })
 export class SessionModule {}
