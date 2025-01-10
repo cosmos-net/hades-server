6d671ca5cd543374852b40a6d5b4a33309da1e94
oztin <roberto.austin@innovationsmi.com>
2025-01-09 21:21:04 -0800

    feat: :sparkles: implement error handling in ActivateInvalidSessionController and update input/output DTOs for session activation

--- src/modules/session/infrastructure/controllers/commands/activate-invalid-session/activate-invalid-session.controller.ts
+++ src/modules/session/infrastructure/controllers/commands/activate-invalid-session/activate-invalid-session.controller.ts
@@ -1,9 +1,10 @@
 import { Controller } from '@nestjs/common';
 import { CommandBus } from '@nestjs/cqrs';
-import { MessagePattern, Payload } from '@nestjs/microservices';
+import { MessagePattern, Payload, RpcException } from '@nestjs/microservices';
 
 import { CMDS_HADES } from '@common/infrastructure/controllers/constants';
-import { ActivateInactiveSessionCommand } from '@session/application/commands/use-cases/activate-invalid-session/activate-invalid-session.command';
+import { ActivateInvalidSessionCommand } from '@session/application/commands/use-cases/activate-invalid-session/activate-invalid-session.command';
+import { SessionModel } from '@session/domain/models/session.model';
 import { ActivateInvalidSessionInputDto } from '@session/infrastructure/controllers/commands/activate-invalid-session/activate-invalid-session.input.dto';
 import { ActivateInvalidSessionOutputDto } from '@session/infrastructure/controllers/commands/activate-invalid-session/activate-invalid-session.output.dto';
 
@@ -15,15 +16,20 @@ export class ActivateInvalidSessionController {
   async update(
     @Payload() activateInactivateSessionInputDto: ActivateInvalidSessionInputDto,
   ): Promise<ActivateInvalidSessionOutputDto> {
-    //TODO: Catch error with try catch and use RpcException to handle error
-    return this.commandBus.execute(
-      new ActivateInactiveSessionCommand({
-        uuid: activateInactivateSessionInputDto.uuid,
-        sessionClosedType: activateInactivateSessionInputDto.sessionClosedType,
-        loggedOutAt: activateInactivateSessionInputDto.loggedOutAt,
-        refreshToken: activateInactivateSessionInputDto.refreshToken,
-        failedAttempts: activateInactivateSessionInputDto.failedAttempts,
-      }),
-    );
+    try {
+      const result = await this.commandBus.execute<ActivateInvalidSessionCommand, SessionModel>(
+        new ActivateInvalidSessionCommand({
+          uuid: activateInactivateSessionInputDto.uuid,
+          sessionDuration: activateInactivateSessionInputDto.sessionDuration,
+          token: activateInactivateSessionInputDto.token,
+          expiresInAt: activateInactivateSessionInputDto.expiresInAt,
+          loggedInAt: activateInactivateSessionInputDto.loggedInAt,
+          refreshToken: activateInactivateSessionInputDto.refreshToken,
+        }),
+      );
+      return new ActivateInvalidSessionOutputDto(result);
+    } catch (error: unknown) {
+      throw new RpcException(error as Error);
+    }
   }
 }
