8c4cda65a69c1032ead94490fc935ba241df7b48
oztin <0315108515@miutt.edu.mx>
2025-01-09 19:30:09 -0800

    refactor: :recycle: rename UpdateSession to ActivateInactiveSession and update related domain services and controllers

--- src/modules/session/domain/domain-services/activate-invalid-session.domain-service.ts
+++ src/modules/session/domain/domain-services/activate-invalid-session.domain-service.ts
@@ -2,11 +2,11 @@ import { SessionStatusEnum } from '@session/domain/constants/session-status.enum
 import { ISessionRepositoryContract } from '@session/domain/contracts/session-repository.contract';
 import { SessionNotValidException } from '@session/domain/exceptions/session-not-valid.exception';
 import { SessionModel } from '@session/domain/models/session.model';
-import { ISessionUpdateFailedPrimitives } from '@session/domain/schemas/session.schema-primitives';
+import { ISessionActivateInactivateSchemaPrimitives } from '@session/domain/schemas/session.schema-primitives';
 
-export class UpdateSessionFailedDomainService {
+export class ActivateInactiveSessionDomainService {
   constructor(private readonly repositorySession: ISessionRepositoryContract) {}
-  async go(schema: ISessionUpdateFailedPrimitives): Promise<SessionModel> {
+  async go(schema: ISessionActivateInactivateSchemaPrimitives): Promise<SessionModel> {
     const sessionModel = await this.repositorySession.getOneBy(schema.uuid);
 
     if (!sessionModel) {
@@ -19,7 +19,7 @@ export class UpdateSessionFailedDomainService {
       );
     }
 
-    sessionModel.changeStatus(SessionStatusEnum.ACTIVE);
+    sessionModel.active();
 
     return sessionModel;
   }
--- src/modules/session/infrastructure/controllers/commands/activate-invalid-session/activate-invalid-session.output.dto.ts
+++ src/modules/session/infrastructure/controllers/commands/activate-invalid-session/activate-invalid-session.output.dto.ts
@@ -1,13 +1,13 @@
-import { ICommand } from '@nestjs/cqrs';
-
-export class UpdateSessionCommand implements ICommand {
+export class ActivateInvalidSessionOutputDto {
+  public readonly id: number;
   public readonly uuid: string;
   public readonly sessionClosedType: string;
   public readonly loggedOutAt: Date;
   public readonly refreshToken: string;
   public readonly failedAttempts: number;
 
-  constructor(root: UpdateSessionCommand) {
+  constructor(root: ActivateInvalidSessionOutputDto) {
+    this.id = root.id;
     this.uuid = root.uuid;
     this.sessionClosedType = root.sessionClosedType;
     this.loggedOutAt = root.loggedOutAt;
