d70441550afbc4c085b567aec5a6c087b219b095
oztin <0315108515@miutt.edu.mx>
2025-01-10 11:53:17 -0800

    feat: :sparkles: implement SessionStatusManager for managing session status transitions and refactor SessionModel to utilize it

--- src/modules/session/domain/models/session.model.ts
+++ src/modules/session/domain/models/session.model.ts
@@ -8,7 +8,6 @@ import UUID from '@common/domain/value-object/vos/uuid.vo';
 import { SessionStatusEnum } from '@session/domain/constants/session-status.enum';
 import { SessionCreatedEvent } from '@session/domain/events/events-success-domain/session-create.event';
 import { SessionStatusChangedEvent } from '@session/domain/events/events-success-domain/session-status-changed.event';
-import { SessionStatusNotChangedException } from '@session/domain/exceptions/session-status-not-changed.exception';
 import { ISessionSchema } from '@session/domain/schemas/session.schema';
 import {
   ISessionBaseSchema,
@@ -146,72 +145,6 @@ export class SessionModel extends AggregateRoot {
     return this._entityRoot?.account;
   }
 
-  private changeStatus(status: SessionStatusEnum): void {
-    const currentStatus = this._entityRoot.status._value;
-
-    this.validateStatusChange(currentStatus, status);
-
-    this._entityRoot.status = new SessionStatus(status);
-    this._entityRoot.updatedAt = new UpdatedAt(new Date());
-
-    this.apply(new SessionStatusChangedEvent(this.toPrimitives()));
-  }
-
-  private validateStatusChange(
-    currentStatus: SessionStatusEnum,
-    newStatus: SessionStatusEnum,
-  ): void {
-    if (currentStatus === newStatus) {
-      throw new SessionStatusNotChangedException(
-        `Session with UUID ${this.uuid} has the same status ${currentStatus}`,
-      );
-    }
-
-    if (this.isFinalStatus(currentStatus)) {
-      throw new SessionStatusNotChangedException(
-        `Session with UUID ${this.uuid} is already ${currentStatus.toLowerCase()}, cannot change status`,
-      );
-    }
-
-    if (this.isInvalidStatusChange(currentStatus, newStatus)) {
-      throw new SessionStatusNotChangedException(
-        `Session with UUID ${this.uuid} is ${currentStatus.toLowerCase()}, cannot change status to ${newStatus.toLowerCase()}`,
-      );
-    }
-  }
-
-  private isFinalStatus(status: SessionStatusEnum): boolean {
-    return [
-      SessionStatusEnum.EXPIRED,
-      SessionStatusEnum.CLOSED,
-      SessionStatusEnum.SUSPENDED,
-    ].includes(status);
-  }
-
-  private isInvalidStatusChange(
-    currentStatus: SessionStatusEnum,
-    newStatus: SessionStatusEnum,
-  ): boolean {
-    const invalidTransitions: { [key in SessionStatusEnum]?: SessionStatusEnum[] } = {
-      [SessionStatusEnum.ACTIVE]: [SessionStatusEnum.INVALID, SessionStatusEnum.PENDING],
-      [SessionStatusEnum.INACTIVE]: [SessionStatusEnum.INVALID, SessionStatusEnum.PENDING],
-      [SessionStatusEnum.INVALID]: [
-        SessionStatusEnum.INACTIVE,
-        SessionStatusEnum.EXPIRED,
-        SessionStatusEnum.CLOSED,
-        SessionStatusEnum.SUSPENDED,
-      ],
-      [SessionStatusEnum.PENDING]: [
-        SessionStatusEnum.INACTIVE,
-        SessionStatusEnum.EXPIRED,
-        SessionStatusEnum.CLOSED,
-        SessionStatusEnum.SUSPENDED,
-      ],
-    };
-
-    return invalidTransitions[currentStatus]?.includes(newStatus) ?? false;
-  }
-
   public hydrate(entity: ISessionSchemaPrimitives): void {
     if (entity.id) this._entityRoot.id = new Id(entity.id);
     if (entity.archivedAt) this._entityRoot.archivedAt = new ArchivedAt(entity.archivedAt);
@@ -307,6 +240,15 @@ export class SessionModel extends AggregateRoot {
   }
 
   public active(): void {
-    this.changeStatus(SessionStatusEnum.ACTIVE);
+    const currentStatus = this._entityRoot.status._value;
+
+    this._entityRoot.status = SessionStatus.createWithTransition(
+      currentStatus,
+      SessionStatusEnum.ACTIVE,
+    );
+
+    this._entityRoot.updatedAt = new UpdatedAt(new Date());
+
+    this.apply(new SessionStatusChangedEvent(this.toPrimitives()));
   }
 }
--- src/modules/session/domain/value-objects/session-status.vo.ts
+++ src/modules/session/domain/value-objects/session-status.vo.ts
@@ -1,5 +1,7 @@
 import { EnumValueObject } from '@common/domain/value-object/types/enum-value-object';
 import { SessionStatusEnum } from '@session/domain/constants/session-status.enum';
+import { SessionStatusNotChangedException } from '@session/domain/exceptions/session-status-not-changed.exception';
+import { SessionStatusManager } from '@session/domain/models/session-status-manager';
 
 export class SessionStatus extends EnumValueObject<SessionStatusEnum> {
   constructor(value: SessionStatusEnum) {
@@ -8,5 +10,20 @@ export class SessionStatus extends EnumValueObject<SessionStatusEnum> {
 
   protected throwErrorForInvalidValue(value: SessionStatusEnum): void {
     throw new Error(`Invalid status: ${value}`);
+  }
+
+  public static createWithTransition(
+    from: SessionStatusEnum,
+    to: SessionStatusEnum,
+  ): SessionStatus {
+    const canTransitionStatus = SessionStatusManager.canTransition(from, to);
+
+    if (!canTransitionStatus) {
+      throw new SessionStatusNotChangedException(
+        `Invalid transition of status from ${from} to ${to}`,
+      );
+    }
+
+    return new SessionStatus(to);
   }
 }
