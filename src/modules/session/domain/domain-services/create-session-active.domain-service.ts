3dbc098d366802851c296459d5eb4bb93af81ac8
2314-RAustin <0315108515@miutt.edu.mx>
2025-01-17 18:14:00 -0800

    feat: :sparkles: add session orchestrator consumer service and contract for improved session management

--- src/modules/session/domain/domain-services/create-session-active.domain-service.ts
+++ src/modules/session/domain/domain-services/create-session-active.domain-service.ts
@@ -1,39 +1,31 @@
 import { SessionStatusEnum } from '@session/domain/constants/session-status.enum';
+import { ISessionOrchestratorConsumerContract } from '@session/domain/contracts/session-orchestrator-consumer.contract';
 import { SessionNotValidException } from '@session/domain/exceptions/session-not-valid.exception';
 import { SessionModel } from '@session/domain/models/session.model';
 import { ISessionBaseSchema } from '@session/domain/schemas/session.schema-primitives';
-import { IAccountRepositoryContract } from '@user/domain/contracts/account-repository.contract';
 
 export class CreateActiveSessionDomainService {
-  constructor(private readonly repositoryAccount: IAccountRepositoryContract) {}
+  constructor(private readonly sessionOrchestratorConsumer: ISessionOrchestratorConsumerContract) {}
 
   async go(sessionBaseSchema: ISessionBaseSchema, accountUUID: string): Promise<SessionModel> {
-    const account = await this.repositoryAccount.getOneBy(accountUUID, {
+    const account = await this.sessionOrchestratorConsumer.account.getByUUID({
+      uuid: accountUUID,
       withArchived: true,
-      include: ['sessions'],
+      failIfArchived: true,
+      includeSessions: true,
     });
-
-    if (!account) {
-      throw new SessionNotValidException(`Account with UUID ${accountUUID} not found`);
-    }
-
-    if (account.archivedAt) {
-      throw new SessionNotValidException(`Account with UUID ${accountUUID} is archived`);
-    }
-
-    //TODO: validate if user is active
 
     const { sessions: sessionList } = account;
 
     if (sessionList) {
       const sessions = sessionList.getItems;
-      const isActiveSession = sessions.find(
+      const session = sessions.find(
         (session): boolean => session.status === SessionStatusEnum.ACTIVE,
       );
 
-      if (isActiveSession) {
+      if (session) {
         throw new SessionNotValidException(
-          `Account with UUID ${accountUUID} already has an active session with UUID ${isActiveSession.uuid}`,
+          `Account with UUID ${accountUUID} already has an active session with UUID ${session.uuid}`,
         );
       }
     }
