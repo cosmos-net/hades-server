513b282aa8c3263786f928e42628bd365d8cb9c3
2314-RAustin <0315108515@miutt.edu.mx>
2025-01-21 23:24:29 -0800

    feat(list-session): refactor filter logic into ListSessionFilter class and update input DTO methods

--- src/modules/session/infrastructure/controllers/queries/list-session/list-session.controller.ts
+++ src/modules/session/infrastructure/controllers/queries/list-session/list-session.controller.ts
@@ -7,6 +7,7 @@ import { ListSessionQuery } from '@session/application/use-cases/queries/list-se
 import { ListSessionModel } from '@session/domain/models/session-list.model';
 import { ListSessionInputDto } from '@session/infrastructure/controllers/queries/list-session/list-session-input.dto';
 import { ListSessionOutputDto } from '@session/infrastructure/controllers/queries/list-session/list-session-output.dto';
+import { ListSessionFilter } from '@session/infrastructure/controllers/queries/list-session/list-session.filter-dto';
 
 @Controller()
 export class ListSessionController {
@@ -16,7 +17,7 @@ export class ListSessionController {
   async list(@Payload() listSessionDto: ListSessionInputDto): Promise<ListSessionOutputDto> {
     try {
       const { orderType, orderBy, page, limit, offset, withArchived } = listSessionDto;
-      const filtersMap = listSessionDto.toFilterMap();
+      const filtersMap = ListSessionFilter.toFilterMap(listSessionDto);
 
       const result = await this.queryBus.execute<ListSessionQuery, ListSessionModel>(
         new ListSessionQuery({
