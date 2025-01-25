779701dbf4cbc8c64758ce90188cf0a8fe4da53c
2314-RAustin <0315108515@miutt.edu.mx>
2025-01-24 20:22:37 -0800

    refactor(role-list-model): add type annotations to improve code clarity and maintainability

--- src/modules/role/domain/models/role-list.model.ts
+++ src/modules/role/domain/models/role-list.model.ts
@@ -9,32 +9,32 @@ export class ListRoleModel {
   private total: number;
 
   constructor(listRole: IListRoleSchemaPrimitive) {
-    this.items = listRole.items.map((role) => new RoleModel(role));
+    this.items = listRole.items.map((role): RoleModel => new RoleModel(role));
     this.total = listRole.total;
   }
 
   public get getItems(): IRoleSchemaPrimitive[] {
-    return this.items.map((role) => role.toPrimitives());
+    return this.items.map((role): IRoleSchemaPrimitive => role.toPrimitives());
   }
 
   public get getTotal(): number {
     return this.total;
   }
 
-  public hydrate(roles: IRoleSchemaPrimitive[]) {
+  public hydrate(roles: IRoleSchemaPrimitive[]): void {
     this.setItems(roles);
   }
 
-  private setTotal(total: number) {
+  private setTotal(total: number): void {
     this.total = total;
   }
 
-  private setItems(items: IRoleSchemaPrimitive[]) {
-    items.forEach((item) => this.addItems(item));
+  private setItems(items: IRoleSchemaPrimitive[]): void {
+    items.forEach((item): void => this.addItems(item));
     this.setTotal(items.length);
   }
 
-  private addItems(item: IRoleSchemaPrimitive) {
+  private addItems(item: IRoleSchemaPrimitive): void {
     this.items.push(new RoleModel(item));
   }
 }
