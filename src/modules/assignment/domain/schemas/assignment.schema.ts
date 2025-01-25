3ce0838b224d7231d66bb63a854eb7f5d820db42
2314-RAustin <0315108515@miutt.edu.mx>
2025-01-24 21:33:27 -0800

    feat(assignment-schema): add IAssignmentSchema interface with value objects

---
+++ src/modules/assignment/domain/schemas/assignment.schema.ts
@@ -0,0 +1,17 @@
+import ArchivedAt from '@common/domain/value-object/vos/archived-at.vo';
+import CreatedAt from '@common/domain/value-object/vos/created-at.vo';
+import Description from '@common/domain/value-object/vos/description.vo';
+import Id from '@common/domain/value-object/vos/id.vo';
+import Title from '@common/domain/value-object/vos/name.vo';
+import UpdatedAt from '@common/domain/value-object/vos/updated-at.vo';
+import UUID from '@common/domain/value-object/vos/uuid.vo';
+
+export interface IAssignmentSchema {
+  id: Id;
+  uuid: UUID;
+  title: Title;
+  description: Description | null;
+  createdAt: CreatedAt;
+  updatedAt: UpdatedAt;
+  archivedAt: ArchivedAt | null;
+}
