d2723544f1762fee91665c541992d7de3a8f4e9e
2314-RAustin <0315108515@miutt.edu.mx>
2025-01-25 12:39:45 -0800

    feat(get-user): add failIfArchived flag to GetUserQuery and update use case

--- src/modules/user/application/use-cases/queries/get-user/get-user.use-case.ts
+++ src/modules/user/application/use-cases/queries/get-user/get-user.use-case.ts
@@ -11,9 +11,9 @@ export class GetUserUseCase implements IQueryHandler<GetUserQuery> {
   constructor(private readonly getUserDomainService: GetUserDomainService) {}
 
   async execute(query: GetUserQuery): Promise<UserAggregate> {
-    const { uuid, withArchived } = query;
+    const { uuid, withArchived, failIfArchived } = query;
 
-    const userAggregate = await this.getUserDomainService.go(uuid, withArchived);
+    const userAggregate = await this.getUserDomainService.go(uuid, withArchived, failIfArchived);
 
     return userAggregate;
   }
