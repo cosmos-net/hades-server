47f7cb546ff2a654c8fae487475b63dd43f819d8
2314-RAustin <0315108515@miutt.edu.mx>
2025-01-27 23:08:19 -0800

    feat(update-assignment-input): enforce conditional validation for roleUUID and description

--- src/modules/assignment/infrastructure/controllers/commands/update-assignment/update-assignment-input.dto.ts
+++ src/modules/assignment/infrastructure/controllers/commands/update-assignment/update-assignment-input.dto.ts
@@ -1,15 +1,15 @@
-import { IsNotEmpty, IsOptional, IsString, IsUUID } from 'class-validator';
+import { IsNotEmpty, IsString, IsUUID, ValidateIf } from 'class-validator';
 
 export class UpdateAssignmentInput {
   @IsUUID()
   public readonly uuid: string;
 
+  @ValidateIf((o): boolean => o.description === undefined)
   @IsUUID()
-  @IsOptional()
   public readonly roleUUID?: string;
 
+  @ValidateIf((o): boolean => o.roleUUID === undefined)
   @IsString()
   @IsNotEmpty()
-  @IsOptional()
   public readonly description?: string;
 }
