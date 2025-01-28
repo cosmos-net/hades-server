cc6661bd8deab7b8161b01b3c9e8f3deb3c9f09e
2314-RAustin <0315108515@miutt.edu.mx>
2025-01-27 19:56:45 -0800

    fix(user-role-assignment): make description optional in UserRoleAssignmentCommand and UserRoleAssignmentInput

--- src/modules/assignment/application/use-cases/commands/user-role-assignment.command.ts
+++ src/modules/assignment/application/use-cases/commands/user-role-assignment.command.ts
@@ -4,12 +4,15 @@ export class UserRoleAssignmentCommand implements ICommand {
   public readonly uuid: string;
   public readonly userUUID: string;
   public readonly roleUUID: string;
-  public readonly description: string;
+  public readonly description?: string;
 
   constructor(root: UserRoleAssignmentCommand) {
     this.uuid = root.uuid;
     this.userUUID = root.userUUID;
     this.roleUUID = root.roleUUID;
-    this.description = root.description;
+
+    if (root.description) {
+      this.description = root.description;
+    }
   }
 }
