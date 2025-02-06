abfce152cac85ff8cd5324ca0016ae660dfabece
2314-RAustin <0315108515@miutt.edu.mx>
2025-02-05 18:52:47 -0800

    feat(microservices): add RabbitMQ support and configuration to the application

--- src/modules/core/infrastructure/framework/main.ts
+++ src/modules/core/infrastructure/framework/main.ts
@@ -11,17 +11,38 @@ import { TransformInterceptor } from '@core/infrastructure/framework/globals/tra
 import { ValidationPipeWithExceptionFactory } from '@core/infrastructure/framework/globals/validation-pipe.global';
 
 async function bootstrap(): Promise<void> {
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(CoreModule, {
+  const app = await NestFactory.create(CoreModule);
+
+  const configService = app.get(ConfigService);
+
+  const natsUrl = configService.get<string>('NATS_URL');
+  Logger.log(`NATS URL: ${natsUrl}`);
+
+  const logger = new Logger('Main');
+
+  app.connectMicroservice<MicroserviceOptions>({
     transport: Transport.NATS,
     options: {
-      servers: [process.env.NATS_URL],
+      servers: [natsUrl],
     },
   });
 
-  const configService = app.get(ConfigService);
-  const natsUrl = configService.get<string>('NATS_URL');
+  const rabbitMqUrl = configService.get<string>('RABBIT_MQ_URL');
+  const rabbitMqQueue = configService.get<string>('RABBIT_MQ_QUEUE');
 
-  Logger.log(`NATS URL: ${natsUrl}`);
+  Logger.log(`Rabbit MQ URL: ${rabbitMqUrl}`);
+  Logger.log(`Rabbit MQ Queue: ${rabbitMqQueue}`);
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.RMQ,
+    options: {
+      urls: [rabbitMqUrl],
+      queue: rabbitMqQueue,
+      queueOptions: {
+        durable: true,
+      },
+    },
+  });
 
   app.useGlobalPipes(
     new ValidationPipeWithExceptionFactory(),
@@ -31,7 +52,10 @@ async function bootstrap(): Promise<void> {
   app.useGlobalInterceptors(new TransformInterceptor(), new TimeOutInterceptor());
   app.useGlobalFilters(new MicroserviceExceptionFilter(), new HttpExceptionFilter());
 
-  await app.listen();
+  const port = configService.get<number>('PORT');
+
+  await app.startAllMicroservices();
+  await app.listen(port, (): void => logger.log(`Application listening on port ${port}`));
 
   process.on('SIGTERM', async (): Promise<void> => {
     await app.close();
