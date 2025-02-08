91350dffa1e0a0edb9b1111a209d46b29864d56c
2314-RAustin <0315108515@miutt.edu.mx>
2025-02-07 19:36:09 -0800

    feat(permission-module): add unarchive permission functionality with DTOs and controller

--- src/modules/permission/infrastructure/controllers/commands/create-permission/create-permission-output.dto.ts
+++ src/modules/permission/infrastructure/controllers/commands/create-permission/create-permission-output.dto.ts
@@ -2,7 +2,10 @@ import { permissionCombinationType } from '@permission/application/use-cases/com
 import { PermissionModel } from '@permission/domain/models/permission.model';
 
 export class CreatePermissionOutputDto {
+  private readonly id: number;
   private readonly uuid: string;
+  private readonly title: string;
+  private readonly description: string;
   private readonly action: permissionCombinationType['action'];
   private readonly module: permissionCombinationType['module'];
   private readonly submodule: permissionCombinationType['submodule'];
@@ -11,7 +14,10 @@ export class CreatePermissionOutputDto {
   private readonly archivedAt?: Date;
 
   constructor(public permission: PermissionModel) {
+    this.id = permission.id;
     this.uuid = permission.uuid;
+    this.title = permission.title;
+    this.description = permission.description;
     this.action = permission.action;
     this.module = permission.module;
     this.submodule = permission.submodule;
