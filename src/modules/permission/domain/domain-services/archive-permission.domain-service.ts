fa1e963b7b2bfea488dc89c556b8cc1bbdcca2ca
Jesús Crespo <crespojesus9@gmail.com>
2025-02-11 19:20:49 -0800

    feat(permission-module): :sparkles: archive permission domain service layer with logic was created

---
+++ src/modules/permission/domain/domain-services/archive-permission.domain-service.ts
@@ -0,0 +1,19 @@
+import { IPermissionRepositoryContract } from '@permission/domain/contracts/permission-repository.contract';
+import { PermissionNotFoundException } from '@permission/domain/exceptions/permission-not-found.exception';
+import { PermissionModel } from '@permission/domain/models/permission.model';
+
+export class ArchivePermissionDomainService {
+  constructor(private readonly permissionRepository: IPermissionRepositoryContract) {}
+
+  async go(uuid: string): Promise<PermissionModel> {
+    const permissionModel = await this.permissionRepository.getOneBy(uuid, { withArchived: true });
+
+    if (!permissionModel) {
+      throw new PermissionNotFoundException(`Permission Not found`);
+    }
+
+    permissionModel.archive();
+
+    return permissionModel;
+  }
+}
