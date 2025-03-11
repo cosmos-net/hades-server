b48ad8bc0d40ab8f4f9b55d54df525841e65d8a2
2314-RAustin <0315108515@miutt.edu.mx>
2025-03-10 23:20:11 -0700

    feat: :sparkles: refactor permission schema interfaces for improved flexibility and clarity

--- src/modules/permission/domain/schemas/permission.schema.ts
+++ src/modules/permission/domain/schemas/permission.schema.ts
@@ -2,7 +2,7 @@ import ArchivedAt from '@common/domain/value-object/vos/archived-at.vo';
 import CreatedAt from '@common/domain/value-object/vos/created-at.vo';
 import Description from '@common/domain/value-object/vos/description.vo';
 import Id from '@common/domain/value-object/vos/id.vo';
-import Title from '@common/domain/value-object/vos/name.vo';
+// import Path from '@common/domain/value-object/vos/name.vo';
 import UpdatedAt from '@common/domain/value-object/vos/updated-at.vo';
 import UUID from '@common/domain/value-object/vos/uuid.vo';
 import { Action } from '@permission/domain/value-objects/action.vo';
@@ -10,13 +10,13 @@ import { Module } from '@permission/domain/value-objects/module.vo';
 import { Submodule } from '@permission/domain/value-objects/submodule.vo';
 
 export interface IPermissionSchema {
-  id: Id;
+  id?: Id;
   uuid: UUID;
-  title: Title;
-  description: Description | null;
+  // path: Path;
+  description?: Description | null;
   createdAt: CreatedAt;
   updatedAt: UpdatedAt;
-  archivedAt: ArchivedAt | null;
+  archivedAt?: ArchivedAt | null;
   action: Action;
   module: Module;
   submodule: Submodule;
