a82940641716d7f3762f501a8d95638baf68578a
2314-RAustin <0315108515@miutt.edu.mx>
2025-03-11 00:08:17 -0700

    feat: :sparkles: update shared module imports: add forwardRef for PolicyModule to resolve circular dependency

--- src/modules/shared/infrastructure/framework/shared.module.ts
+++ src/modules/shared/infrastructure/framework/shared.module.ts
@@ -1,29 +1,23 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
 
 import { PermissionModule } from '@permission/infrastructure/framework/permission.module';
+import { PolicyModule } from '@policy/infrastructure/framework/policy.module';
 import { RoleModule } from '@role/infrastructure/framework/role.module';
-import { SHARED_DATA_MEDIATOR_SERVICE } from '@shared/domain/constants/shared-injection-tokens.constants';
 import { DataMediatorService } from '@shared/infrastructure/services/data-mediator-service/data-mediator.service';
 import { PolicyAttacherProgramService } from '@shared/infrastructure/services/saga-orchestrator/policy/policy-attacher-program/policy-attacher-program.service';
 import { PolicyDetacherProgramService } from '@shared/infrastructure/services/saga-orchestrator/policy/policy-detacher-program/policy-detacher-program.service';
 import { UserModule } from '@user/infrastructure/framework/user.module';
-import { UserAggregateFacadeService } from '@user/infrastructure/services/facade/user-aggregate-facade.service';
-import { UserFacadeService } from '@user/infrastructure/services/facade/user-facade.service';
 
 @Module({
-  imports: [CqrsModule, RoleModule, UserModule, PermissionModule],
-  providers: [
-    DataMediatorService,
-    PolicyAttacherProgramService,
-    PolicyDetacherProgramService,
-    // UserFacadeService,
-    // UserAggregateFacadeService,
-    // {
-    //   provide: SHARED_DATA_MEDIATOR_SERVICE,
-    //   useClass: DataMediatorService,
-    // },
+  imports: [
+    CqrsModule,
+    RoleModule,
+    UserModule,
+    PermissionModule,
+    forwardRef((): typeof PolicyModule => PolicyModule),
   ],
+  providers: [DataMediatorService, PolicyAttacherProgramService, PolicyDetacherProgramService],
   controllers: [],
   exports: [DataMediatorService, PolicyAttacherProgramService, PolicyDetacherProgramService],
 })
