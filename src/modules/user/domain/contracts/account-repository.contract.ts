58eb03fd67e103908176a76736efbff199b605b1
2314-RAustin <0315108515@miutt.edu.mx>
2025-03-25 13:50:08 -0700

    feat: :sparkles: implement search methods in account repository: add searchByEmail and searchByUsername methods to IAccountRepositoryContract and AccountTypeormRepository

--- src/modules/user/domain/contracts/account-repository.contract.ts
+++ src/modules/user/domain/contracts/account-repository.contract.ts
@@ -1,6 +1,7 @@
 import { IOptions } from '@common/domain/contracts/options.contract';
 import { AccountModel } from '@user/domain/models/account/account.model';
-
 export abstract class IAccountRepositoryContract {
   abstract getOneBy(UsernameOrEmailOrUUID: string, options?: IOptions): Promise<AccountModel>;
+  abstract searchByEmail(email: string, options?: IOptions): Promise<AccountModel>;
+  abstract searchByUsername(username: string, options?: IOptions): Promise<AccountModel>;
 }
