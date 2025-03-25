f0d794c80036e2c46eeb2dc6ab0d0c89bd078b20
2314-RAustin <0315108515@miutt.edu.mx>
2025-03-25 13:50:58 -0700

    feat: :sparkles: implement search by email functionality: add input/output DTOs and controller for searching accounts by email

---
+++ src/modules/user/infrastructure/controllers/queries/search-account-by-email/search-account-by-email-output.dto.ts
@@ -0,0 +1,23 @@
+import { DeepPartial } from '@helpers/types/partials.helper';
+import { AccountModel } from '@user/domain/models/account/account.model';
+import { IAccountSchemaPrimitives } from '@user/domain/schemas/account/account.schema-primitive';
+
+export class SearchAccountByEmailOutputDto implements DeepPartial<IAccountSchemaPrimitives> {
+  id?: number;
+  uuid: string;
+  username: string;
+  email: string;
+  createdAt: Date;
+  updatedAt: Date;
+  archivedAt?: Date;
+
+  constructor(accountModel: AccountModel) {
+    this.id = accountModel.id;
+    this.uuid = accountModel.uuid;
+    this.username = accountModel.username;
+    this.email = accountModel.email;
+    this.createdAt = accountModel.createdAt;
+    this.updatedAt = accountModel.updatedAt;
+    this.archivedAt = accountModel.archivedAt;
+  }
+}
